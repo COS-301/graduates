c1331e7adec438ad510a6183f0b85fc2b20d7021
Muziwandile <84350184+muzicoding@users.noreply.github.com>
2022-03-20 15:58:00 +0200

    👽️ 🤡 ✨ (Graphql): Added Storage Mock Api ,And linked the MOdule with The Shell

--- libs/api/shell/feature/src/lib/api-shell-feature.module.ts
+++ libs/api/shell/feature/src/lib/api-shell-feature.module.ts
@@ -1,7 +1,13 @@
 import { FeatureModule as ApiExampleFeatureModule } from '@graduates/api/example/api/feature';
+import { ApiStorageApiFeatureModule as ApiStorageFeatureModule } from '@graduates/api/storage/api/feature';
 import { Module } from '@nestjs/common';
-
+import {GraphQLModule} from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 @Module({
-  imports: [ApiExampleFeatureModule],
+  imports: [ApiExampleFeatureModule,
+  GraphQLModule.forRoot({
+    autoSchemaFile: true,
+    driver: ApolloDriver
+  }), ApiStorageFeatureModule],
 })
 export class ApiShellFeatureModule {}
