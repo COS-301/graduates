6f8ed83693fc3c1eef90db7717a05b2c925e536a
Michael du Plessis <mdp0101@gmail.com>
2022-03-23 08:37:37 +0200

    :pencil2: fixed typo of class member UserID -> userID

--- libs/api/access-status/api/feature/src/lib/api-access-status.service.ts
+++ libs/api/access-status/api/feature/src/lib/api-access-status.service.ts
@@ -5,7 +5,7 @@ import { ApiAccessStatusEntity } from "./api-access-status.entity";
 export class ApiAccessStatusService {
     async getAll(): Promise<ApiAccessStatusEntity[]> {
         const accessStatus = new ApiAccessStatusEntity();
-        accessStatus.UserID = "u04515146";
+        accessStatus.userID = "u04515146";
         accessStatus.item = "CV";
         accessStatus.accessStatus = "Pending"
 
