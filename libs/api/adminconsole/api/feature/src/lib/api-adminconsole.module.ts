2c7274682edbe1ef9e24c8ceb64bb8b3889fd599
Thebe <u18371435@tuks.co.za>
2022-03-23 23:16:32 +0200

    (adminconsole)api mock

---
+++ libs/api/adminconsole/api/feature/src/lib/api-adminconsole.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { ApiAdminconsoleApiFeatureModule } from '@graduates/api/adminconsole/service/feature';
+import { ApiAdminConsoleResolver } from "./api-adminconsole.resolver";
+
+@Module({
+    controllers:[],
+    providers: [ApiAdminConsoleResolver,ApiAdminconsoleApiFeatureModule],
+    exports:[],
+})
+export class ApiAdminconsoleApiFeatureModule{}
