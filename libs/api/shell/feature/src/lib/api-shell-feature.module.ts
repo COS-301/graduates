99a89ef1a501a54935c053ea7b52885d8ee60ed6
takumuguti <90533439+takumuguti@users.noreply.github.com>
2022-03-23 13:38:43 +0200

    🤡(hosting): Mock API for status page
    created the mock API required for the hosting status page.

--- libs/api/shell/feature/src/lib/api-shell-feature.module.ts
+++ libs/api/shell/feature/src/lib/api-shell-feature.module.ts
@@ -1,9 +1,10 @@
 import { FeatureModule as ApiExampleFeatureModule } from '@graduates/api/example/api/feature';
 import { ApiStorageApiFeatureModule as ApiStorageFeatureModule } from '@graduates/api/storage/api/feature';
+import { ApiHostingApiFeatureModule as ApiHostingFeatureModule } from '@graduates/api/hosting/api/feature';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { ApiShortsApiFeatureModule as ApiShortsFeatureModule } from '@graduates/api/shorts/api/feature';
+//import { ApiShortsApiFeatureModule as ApiShortsFeatureModule } from '@graduates/api/shorts/api/feature';
 @Module({
   imports: [
     ApiExampleFeatureModule,
@@ -12,7 +13,8 @@ import { ApiShortsApiFeatureModule as ApiShortsFeatureModule } from '@graduates/
       driver: ApolloDriver,
     }),
     ApiStorageFeatureModule,
-    ApiShortsFeatureModule,
+    //ApiShortsFeatureModule,
+    ApiHostingFeatureModule,
   ],
 })
 export class ApiShellFeatureModule {}
