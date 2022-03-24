af9ecbe4df767383e73f9771584f4078b656b52b
Michael du Plessis <mdp0101@gmail.com>
2022-03-24 15:30:31 +0200

    :bug: added request-access feature to shell and added request-access feature and service to the corresponding module

--- libs/api/request-access/api/feature/src/lib/api-request-access.module.ts
+++ libs/api/request-access/api/feature/src/lib/api-request-access.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ApiRequestAccessResolver } from './api-request-access.resolver';
+import { ApiRequestAccessService } from './api-request-access.service';
 
 @Module({
   controllers: [],
-  providers: [],
+  providers: [ApiRequestAccessResolver, ApiRequestAccessService],
   exports: [],
 })
 export class ApiRequestAccessApiFeatureModule {}
