29042aff43aa47f5089bd5396acd1cff842f1451
DamianJordaan <u20473509@tuks.co.za>
2022-03-26 23:14:40 +0200

    🤡 🚑 (StudentExplore-api) Mock API V1-F Hotfix

--- libs/api/student-explore/api/feature/src/lib/api-student-explore.module.ts
+++ libs/api/student-explore/api/feature/src/lib/api-student-explore.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ApiStudentExploreResolver } from './api-student-explore.resolver';
+import { ApiStudentExploreServiceFeatureModule } from '@graduates/api/student-explore/service/feature';
 
 @Module({
   controllers: [],
-  providers: [],
+  providers: [ApiStudentExploreResolver, ApiStudentExploreServiceFeatureModule],
   exports: [],
 })
 export class ApiStudentExploreApiFeatureModule {}
