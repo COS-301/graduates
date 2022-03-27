4b4bbedb26b0a65073087590b40a9648ef0f5484
MoeMoola3 <73942516+MoeMoola3@users.noreply.github.com>
2022-03-27 15:33:03 +0200

    🚧 (ts): Updated Module file

--- libs/api/authentication/api/feature/src/lib/api-authentication-api-feature.module.ts
+++ libs/api/authentication/api/feature/src/lib/api-authentication-api-feature.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 import { AppController } from './api-authentication-api-feature.controller';
 import { AppService } from './api-authentication-api-feature.service';
 import { UsersModule } from './users/users.module';
