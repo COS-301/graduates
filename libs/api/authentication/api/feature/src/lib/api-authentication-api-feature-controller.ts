135d5dff4a13ef7f9150b9f2cd7334e91883a37b
MxolisiMkanzi <u18059326@tuks.co.za>
2022-03-29 22:24:24 +0200

    Implemented the login and callback functions

--- libs/api/authentication/api/feature/src/lib/api-authentication-api-feature-controller.ts
+++ libs/api/authentication/api/feature/src/lib/api-authentication-api-feature-controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { ApiAuthenticationServiceFeatureModule } from 'libs/api/authentication/service/feature/src/lib/api-authentication-service-feature.module';
 
@@ -11,4 +12,19 @@ export class ApiAuthenticationApiFeatureController {
     getHello(@Body(): body: customDTO, @Param('id') userId: String):String{
         return this.service.getHello();
     }*/
+
+    @Get('authenticate')
+    @UseGuards(AuthGuard('google'))
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    async authenticate(@Req() req){
+        return "Authenticate";
+    }
+
+    @Get('authenticate/google/callback')
+    @UseGuards(AuthGuard('google'))
+    authRedirect(@Req() req){
+        return this.service.googleLogin(req);
+    }
+    
+
 }
