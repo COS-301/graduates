05e507e2fa318b103be6e9f2a9431d36022dcca0
MxolisiMkanzi <u18059326@tuks.co.za>
2022-03-29 13:25:53 +0200

    Updated DTOs

--- libs/api/authentication/api/feature/src/lib/api-authentication-api-feature-controller.ts
+++ libs/api/authentication/api/feature/src/lib/api-authentication-api-feature-controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { ApiAuthenticationServiceFeatureModule } from 'libs/api/authentication/service/feature/src/lib/api-authentication-service-feature.module';
 
 @Controller('api-authentication-api-feature')
