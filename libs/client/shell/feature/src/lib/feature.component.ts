260048d0c3c9a72fd32f7ef5fd78f466cf6a7be6
bevanslabbert <bevanslabbert@gmail.com>
2022-03-30 16:26:24 +0200

    Admin Console Client Page
    HTML for admin console. (No components as of yet)

--- libs/client/shell/feature/src/lib/feature.component.ts
+++ libs/client/shell/feature/src/lib/feature.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'graduates-shell-feature',
   templateUrl: './feature.component.html',
   styleUrls: ['./feature.component.scss'],
 })
-export class FeatureComponent {}
+export class FeatureComponent{
+
+  sidenavOptions : string[] 
+  option : string
+
+  constructor() {
+    //Populate the sidenav with these options
+    this.sidenavOptions = ["Blogs", "Create User", "Users", "Story", "Roles", "Blogs", "Shorts"]
+
+    //Set default option
+    this.option = this.sidenavOptions[0]
+
+  }
+ 
+
+  sidenavOption(option : string) {
+    this.option = option
+  }
+}
