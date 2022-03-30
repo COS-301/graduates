93ec9c1786dd72032ccea01e0c6063549f5cb072
bevanslabbert <bevanslabbert@gmail.com>
2022-03-30 18:41:31 +0200

    Admin Console Client : Blogs Page
    (No components as of yet)

--- libs/client/shell/feature/src/lib/feature.component.ts
+++ libs/client/shell/feature/src/lib/feature.component.ts
@@ -11,14 +11,15 @@ export class FeatureComponent{
   option : string
   users : any[]
   currentUser : any
-
+  allRoles : string[]
   constructor() {
     //Populate the sidenav with these options
-    this.sidenavOptions = ["Roles", "Create User", "Users", "Story", "Roles", "Blogs", "Shorts"]
+    this.sidenavOptions = ["Users", "Create User", "Users", "Story", "Roles", "Blogs", "Shorts"]
 
     //Set default option
     this.option = this.sidenavOptions[0]
     this.users = []
+    this.allRoles = []
     this.fetchData()
   }
  
@@ -27,11 +28,16 @@ export class FeatureComponent{
       case "Blogs" :
         this.users = [{"name" : "Jack", "archived" : true},  {"name" : "Angela", "archived" : false}, {"name" : "Marceline", "archived" : true},{"name" : "Jonathan", "archived" : false}]
         break
+      case "Users" :
+        this.users = [{"name" : "Jack", "archived" : true},  {"name" : "Angela", "archived" : false}, {"name" : "Marceline", "archived" : true},{"name" : "Jonathan", "archived" : false}]
+        break
       case "Roles" :
         this.users = [{"name" : "Jack", "roles" : ["A", "B"]}, {"name" : "Angela", "roles" : ["A", "D"]}, {"name" : "Marceline", "roles" : ["A","B", "C"]},{"name" : "Jonathan", "roles" : ["D"]}]
         this.currentUser = this.users[0]
         break
     }
+    
+    this.allRoles = ["Role 1", "Role 2", "Role 3", "Role 4", "Role 5"]
   }
 
   sidenavOption(option : string) {
@@ -43,6 +49,11 @@ export class FeatureComponent{
 
   removeRole(role : string) {
     //Remove user's role
+
+  }
+
+  addRole(role:  string){
+    //add role to user
 
   }
 }
