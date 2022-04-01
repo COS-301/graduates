2ab1a808906d5e4fd148b2c3ba86b887cfa1f17b
MoeMoola3 <73942516+MoeMoola3@users.noreply.github.com>
2022-04-02 00:53:36 +0200

    🔥(ts): Removed main file

--- libs/api/authentication/api/feature/src/lib/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { ApiAuthenticationApiFeatureModule } from './api-authentication-api-feature.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(ApiAuthenticationApiFeatureModule);
-  await app.listen(3000);
-}
-bootstrap();
