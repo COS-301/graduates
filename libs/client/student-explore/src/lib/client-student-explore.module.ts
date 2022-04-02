f76a62643d6a2a3a202b12b1d3b59a521e51d424
mattreed01 <matthewreed0107@gmail.com>
2022-04-02 12:34:36 +0200

    Created a filter component

--- libs/client/student-explore/src/lib/client-student-explore.module.ts
+++ libs/client/student-explore/src/lib/client-student-explore.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    FilterComponent
+  ],
 })
 export class ClientStudentExploreModule {}
---
+++ libs/client/student-explore/src/lib/filter/filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'graduates-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.scss']
+})
+export class FilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
