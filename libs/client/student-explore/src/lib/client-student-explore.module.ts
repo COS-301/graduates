91fec09ca7acfcad7106bd2390a846b0ebd9336c
mattreed01 <matthewreed0107@gmail.com>
2022-04-02 12:11:10 +0200

    Created student-explore feature library

--- libs/client/student-explore/src/lib/client-student-explore.module.ts
+++ libs/client/student-explore/src/lib/client-student-explore.module.ts
@@ -4,4 +4,4 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   imports: [CommonModule],
 })
-export class ClientStudentExploreStudentExploreModule {}
+export class ClientStudentExploreModule {}
