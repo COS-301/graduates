ec24228357600a6f0b7466125b7f433456b547b0
Kuda214 <kudachivunga33@gmail.com>
2022-04-02 09:30:47 +0200

    ✨(api) Added service in resolver.ts

--- libs/api/authorization/api/feature/src/lib/api-authorization.resolver.spec.ts
+++ libs/api/authorization/api/feature/src/lib/api-authorization.resolver.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApiAuthorizationResolver } from './api-authorization.resolver';
+import { ApiAuthorizationService } from '@graduates/api/authorization/service/feature'
 
 describe('ApiAuthorizationResolver', () => {
   let resolver: ApiAuthorizationResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ApiAuthorizationResolver],
+      providers: [ApiAuthorizationResolver,ApiAuthorizationService],
     }).compile();
 
     resolver = module.get<ApiAuthorizationResolver>(ApiAuthorizationResolver);
