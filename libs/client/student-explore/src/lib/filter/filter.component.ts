2d874beb04e92dd322b5df251e6adee36d6dedd5
mattreed01 <matthewreed0107@gmail.com>
2022-04-03 21:57:27 +0200

    💄  (ui): Added student-explore filter component

--- libs/client/student-explore/src/lib/filter/filter.component.ts
+++ libs/client/student-explore/src/lib/filter/filter.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
-  selector: 'graduates-filter',
+  selector: 'graduates-student-explore-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  styleUrls: ['./filter.component.scss'],
+  providers: [MatCheckboxModule, MatMenuModule, MatButtonModule, MatIconModule]
 })
 export class FilterComponent implements OnInit {
 
