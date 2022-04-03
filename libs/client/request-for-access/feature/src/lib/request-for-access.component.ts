ef082f756c84f04d8706e5e5fd299f46101ea543
dylan2894 <61905387+dylan2894@users.noreply.github.com>
2022-04-03 09:27:31 +0200

    ✨(ui): Adding request-for-access feature

---
+++ libs/client/request-for-access/feature/src/lib/request-for-access.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'graduates-request-for-access-feature',
+  templateUrl: './request-for-access.component.html',
+  styleUrls: ['./request-for-access.component.scss'],
+})
+export class RequestForAccessComponent {}
---
+++ libs/client/request-for-access/feature/src/lib/request-for-access.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RequestForAccessComponent } from './request-for-access.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [RequestForAccessComponent],
+})
+export class FeatureModule {}
