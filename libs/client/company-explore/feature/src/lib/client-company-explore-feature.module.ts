2a8272966a206eed1640d349f3bedbead75259b4
Trunks29 <u17169811@tuks.co.za>
2022-04-04 12:13:49 +0200

    material module
    added a angulater
     material module ts, for single point to import materials

--- libs/client/company-explore/feature/src/lib/client-company-explore-feature.module.ts
+++ libs/client/company-explore/feature/src/lib/client-company-explore-feature.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { CompanyExploreFeatureComponent } from './company-explore-feature/company-explore-feature.component';
+import{CompanyExploreMaterialModule} from './materials/company-explore-material.module';
 
 export const clientCompanyExploreFeatureRoutes: Route[] = [];
 
