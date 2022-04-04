15c76529764002cf57ba15c1aae808666d9a7b2c
Trunks29 <u17169811@tuks.co.za>
2022-04-04 12:21:13 +0200

    Update client-company-explore-feature.module.ts

--- libs/client/company-explore/feature/src/lib/client-company-explore-feature.module.ts
+++ libs/client/company-explore/feature/src/lib/client-company-explore-feature.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { CompanyExploreFeatureComponent } from './company-explore-feature/company-explore-feature.component';
 import{CompanyExploreMaterialModule} from './materials/company-explore-material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 export const clientCompanyExploreFeatureRoutes: Route[] = [];
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule,
+    RouterModule,
+    CompanyExploreMaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   declarations: [
     CompanyExploreFeatureComponent
   ],
