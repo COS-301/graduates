ffbf5f031ff1d68578f9b4239d4ab6aa22f607cf
Leonardo Wessels u17229457 <u17229457@tuks.co.za>
2022-04-06 17:25:25 +0200

    🗃️ (database):
    reformed the skeletons to more accurately represent the repository

--- libs/api/authentication/repository/data-access/src/lib/data-access.ts
+++ libs/api/authentication/repository/data-access/src/lib/data-access.ts
@@ -1,115 +1,110 @@
 //Following handy tutorial from : https://www.sitepoint.com/using-redis-node-js/
 
 //add where you wish to use the code
-import jwt from "jsonwebtoken";
 import {PrismaClient} from '@prisma/client';
+import internal = require('stream');
 
 class TokenAuthenticate 
 {
     prisma = new PrismaClient();
-    jwt = new jwt();
-
-    //const redis = require("redis");
-    //For safety we will need to change port and host in redis const client = redis.createClient(port, host); in the form "127.0.0.1" host "6379"
 
     /**
      * 
-     * REDIS DATBASE WILL CONTAIN USERNAME, TOKEN, TOKEN EXPIRATION
+     * @param username 
+     * @param password 
+     * @returns 
      */
-
-
-
-    /**
-     * @param username users username
-     * @param password users password
-     * @returns JWT formatted return token for client side storage
-     * @description Main function to authenticate a user
-     */
-    authenticate(username: string, password: string): JSON {
-        //TODO: Password Hashing Algorithm
-        //TODO: Verify Against Main DB
-        //TODO: Add user to redis DB with auth token
-        //TODO: Return JWT Token
-        let JWTToken: JSON;
-        return JWTToken;
-
-    }
-
-    /**
-     * 
-     * @param gAuth takes in the JSON result from the google auth 
-     * @description Sets up 
-     */
-    googleAuthenticate(gAuth: JSON) {
-        const username: string = gAuth["user"]["firstName"];
-        const token: string = gAuth["user"]["accessTokens"];
-        const tokenExpiration: Date = this.getDate(7);
-        const returner = this.client.hSet(username, { 'token_details': { 'token': token, 'token_expiration': tokenExpiration } });
-        return returner;
-    }
-
-    /**
-     * 
-     * @param token new token
-     * @param username user to change the token of
-     * @returns updated JWT token
-     * @description Called by authenticate. Take in verified username and assigns this user a token. The user, 
-     *              token and expiration date of the token are then stored in our redis database. 
-     */
-    setToken(token: string, username: string) {
-        //TODO: create token to set username to
-        const signed = this.jwt.sign({ data: token }, "TESTPRIVATEKEY", { algorithm: 'RS256' });
-        return signed;
-    }
-
-    /**
-     *  
-     * @param username user name to find token
-     * @returns token expiration time
-     */
-    getTokenExpiration(username: string) {
-        //TODO: get time until token expires with redis
-        let expirationTime: JSON;
-        return expirationTime;
-    }
-
-    /**
-     * 
-     * @param username user name to be used as key to set token time to current time
-     */
-
-    requestTokenReset(username: string) {
-        //TODO: set time of username to current time so next authorization request returns false and requires user to resin in 
-    }
-
-    getDate(days: number) {
-        const date: Date = new Date();
-        date.setDate(date.getDate() + days);
-        return date;
-    }
-
-    /**
-     * 
-     * @returns current time as int
-     */
-    getDateNow() {
-        //TODO: implement time function
-        const date: Date = new Date;
-        return date;
-    }
-
-
-
-    /*
-
-    Proposed JWT token in the form
-
+    setNewUser(username : string, password : string)
     {
-        "username" : username,
-        "token_value" : token,
-        "user_id" : userId   
+         return username;
     }
 
-    */
+    /**
+     * 
+     * @param username 
+     * @returns 
+     */
+    getPassword(username : string)
+    {
+        return username;
+    }
+
+
+    /**
+     * 
+     * @param username 
+     * @param newPassword 
+     * @returns 
+     */
+    setPassword(username  : string, newPassword :string)
+    {
+        return username;
+    }
+
+    /**
+     * 
+     * @param username 
+     * @param token 
+     * @param time 
+     * @param token_type 
+     * @returns 
+     */
+    setToken(username : string, token : string, time : internal, token_type : number)
+    {
+        return username;
+    }
+
+    /**
+     * 
+     * @param username 
+     * @returns 
+     */
+    getToken(username : string)
+    {
+        return username;
+    }
+
+    /**
+     * 
+     * @param username 
+     * @param time 
+     * @returns 
+     */
+    setTokenExpiration(username : string, time : number)
+    {
+        return username;
+    }
+
+    /**
+     * 
+     * @param username 
+     * @param token_type 
+     * @returns 
+     */
+    getTokenExpiration(username : string, token_type : number)
+    {
+        return username;
+    }
+
+    /**
+     * 
+     * @param username 
+     * @param token_type 
+     * @returns 
+     */
+    setTokenType(username : string, token_type : number)
+    {
+        return username;
+    }
+
+    /**
+     * 
+     * @param username 
+     * @returns 
+     */
+    getTokenType(username : string)
+    {
+        return username;
+    }
 }
 
