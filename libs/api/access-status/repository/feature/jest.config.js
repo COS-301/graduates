f7d22aebf51e8ba08a7de7782626c10eec70c00d
Farai <farai.chivunga.112@gmail.com>
2022-04-06 20:02:25 +0200

    ✨  (api): Added request for access status scaffolding

---
+++ libs/api/access-status/repository/feature/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  displayName: 'api-access-status-repository-feature',
+  preset: '../../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory:
+    '../../../../../coverage/libs/api/access-status/repository/feature',
+};
