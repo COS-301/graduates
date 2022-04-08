5ee60fdf23cf9317a101ab449dcf6506ea6fddab
Dylan <whiteford957@gmail.com>
2022-04-08 03:11:34 +0200

    Reconfigured Shorts/feature folders

---
+++ libs/client/shorts/feature/src/lib/feature.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FeatureComponent } from './feature.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [FeatureComponent],
+})
+export class FeatureModule {}
