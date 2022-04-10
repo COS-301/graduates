0b6b10828590019db335302d203b6a615ccd0d06
MuziwandileTNdlovu <84350184+MuziwandileTNdlovu@users.noreply.github.com>
2022-04-10 22:34:00 +0200

    Update api-storage.entity.ts

--- libs/api/storage/api/shared/data-access/src/lib/api-storage.entity.ts
+++ libs/api/storage/api/shared/data-access/src/lib/api-storage.entity.ts
@@ -3,11 +3,11 @@ import { Field, ID, ObjectType } from '@nestjs/graphql';
 @ObjectType()
 export class ApiStorage {
   @Field(() => ID)
-  userId!: String;
-  @Field(()=> String)
-  fileCategory!: String;
-  @Field(() => String)
-  fileExtension!: String;
-  @Field(()=>String)
-  filePath!: String;
+  userId!: string;
+  @Field()
+  fileCategory!: string;
+  @Field()
+  fileExtension!: string;
+  @Field()
+  filePath!: string;
 }
