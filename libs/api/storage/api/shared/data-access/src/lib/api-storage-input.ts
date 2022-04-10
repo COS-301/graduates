41cee2f187f96456b4a8836242e9096a19f6b551
MuziwandileTNdlovu <84350184+MuziwandileTNdlovu@users.noreply.github.com>
2022-04-10 22:32:48 +0200

    Update api-storage-input.ts

--- libs/api/storage/api/shared/data-access/src/lib/api-storage-input.ts
+++ libs/api/storage/api/shared/data-access/src/lib/api-storage-input.ts
@@ -4,11 +4,11 @@ import { Field, ID, InputType } from '@nestjs/graphql';
 @InputType()
 export class ApiStorageInput {
   @Field(() => ID)
-  userId!: String;
-  @Field(()=> String)
-  fileCategory!: String;
-  @Field(() => String)
-  fileExtension!: String;
-  @Field(()=>String)
-  filePath!: String;
+  userId!: string;
+  @Field()
+  fileCategory!: string;
+  @Field()
+  fileExtension!: string;
+  @Field()
+  filePath!: string;
 }
