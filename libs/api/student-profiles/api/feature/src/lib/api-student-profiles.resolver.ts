814e0c9ae1428852e3c1c6bd2b2586238ecdbc2f
Xander <u20465026@tuks.co.za>
2022-04-11 01:46:21 +0200

    ✨ (student-profiles):Added API stubs

--- libs/api/student-profiles/api/feature/src/lib/api-student-profiles.resolver.ts
+++ libs/api/student-profiles/api/feature/src/lib/api-student-profiles.resolver.ts
@@ -1,13 +1,12 @@
-import { Query, Args, Resolver } from '@nestjs/graphql';
+import { Query, Args, Resolver, Mutation } from '@nestjs/graphql';
 import { ApiStudentProfilesEntity } from '@graduates/api/student-profiles/api/shared/data-access';
 import { ApiStudentProfileService } from '@graduates/api/student-profiles/service/feature';
 
-
 @Resolver((of) => ApiStudentProfilesEntity)
 export class ApiStudentProfileResolver {
-   constructor(private studentService: ApiStudentProfileService) {}
+  constructor(private studentService: ApiStudentProfileService) {}
 
-  @Query((returns) => ApiStudentProfilesEntity,{ name: 'student' })
+  @Query((returns) => ApiStudentProfilesEntity, { name: 'student' })
   async getStudent(@Args('studentNum', { type: () => String }) id: string) {
     const studentArr = this.studentService.findOneById(id);
     const studentObj = new ApiStudentProfilesEntity();
@@ -19,5 +18,25 @@ export class ApiStudentProfileResolver {
     studentObj.lastName = (await studentArr).pop();
 
     return studentObj;
+  }
+
+  @Mutation((returns) => ApiStudentProfilesEntity)
+  async editStudent(@Args('studentNum', { type: () => String }) id: string) {
+    const studentArr = this.studentService.findOneById(id);
+    const studentObj = new ApiStudentProfilesEntity();
+    studentObj.dateOfBirth = (await studentArr).pop();
+    studentObj.phoneNum = (await studentArr).pop();
+    studentObj.email = (await studentArr).pop();
+    studentObj.firstName = (await studentArr).pop();
+    studentObj.studentNum = (await studentArr).pop();
+    studentObj.lastName = (await studentArr).pop();
+
+    return studentObj;
+  }
+
+  @Mutation((returns) => String)
+  async deleteStudent() {
+    const res = 'Delete not implemented';
+    return res;
   }
 }
--- libs/api/student-profiles/api/shared/data-access/src/lib/api-student-profiles.entity.ts
+++ libs/api/student-profiles/api/shared/data-access/src/lib/api-student-profiles.entity.ts
@@ -1,15 +1,18 @@
-import { Field, Int, ObjectType } from "@nestjs/graphql";
+import { Field, Int, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
-export class ApiStudentProfilesEntity{
+export class ApiStudentProfilesEntity {
   @Field()
-  studentNum: string
+  studentNum: string;
 
   @Field()
   firstName: string;
 
   @Field()
   lastName: string;
+
+  @Field()
+  title: string;
 
   @Field()
   email: string;
@@ -20,6 +23,34 @@ export class ApiStudentProfilesEntity{
   @Field()
   dateOfBirth: string;
 
-  @Field(type => [Int])
-  marks: number[];
+  @Field()
+  nameOfDegree: string;
+
+  @Field()
+  bio: string;
+
+  @Field()
+  tags: string[];
+
+  @Field()
+  preferredLocation: string;
+
+  @Field()
+  employmentStatus: string;
+
+  @Field()
+  notableAchievements: string[];
+
+  @Field()
+  links: string[];
+
+  //uploaded documents return if they are uploaded or not
+  @Field()
+  academicRecord: boolean;
+
+  @Field()
+  cv: boolean;
+
+  @Field()
+  capstoneProject;
 }
