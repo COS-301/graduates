6bcc9c4e20d692f0bac3770320b0ab1595a483b2
Dylan Whitefod <u19214597@tuks.co.za>
2022-04-11 00:36:26 +0200

    :art: rebuilt shorts UI lib and added report-menu component to it

--- libs/client/shorts/feature/src/lib/client-shorts-feature.module.ts
+++ libs/client/shorts/feature/src/lib/client-shorts-feature.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReportMenuComponent } from './report-menu/report-menu.component';
 
 @NgModule({
   imports: [
@@ -9,6 +10,12 @@ import { RouterModule } from '@angular/router';
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
+  ],
+  declarations: [
+    ReportMenuComponent
+  ],
+  exports: [
+    ReportMenuComponent
   ],
 })
 export class ClientShortsFeatureModule {}
---
+++ libs/client/shorts/feature/src/lib/report-menu/report-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'graduates-report-menu',
+  templateUrl: './report-menu.component.html',
+  styleUrls: ['./report-menu.component.scss']
+})
+export class ReportMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
