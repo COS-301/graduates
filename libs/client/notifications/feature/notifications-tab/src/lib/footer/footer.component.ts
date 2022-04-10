8f2aa22107066ac7184401aec2731bacff099815
fffrikkie <u20532581@tuks.co.za>
2022-04-10 22:42:01 +0200

    Generated libs correctly

--- libs/client/notifications/feature/notifications-tab/src/lib/footer/footer.component.ts
+++ libs/client/notifications/feature/notifications-tab/src/lib/footer/footer.component.ts
@@ -5,11 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    //do something
   }
 
 }
--- libs/client/notifications/feature/notifications-tab/src/lib/notif-display/notif-display.component.ts
+++ libs/client/notifications/feature/notifications-tab/src/lib/notif-display/notif-display.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'graduates-notif-display',
   templateUrl: './notif-display.component.html',
   styleUrls: ['./notif-display.component.scss']
 })
-export class NotifDisplayComponent implements OnInit {
+export class NotifDisplayComponent  {
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  
 
 }
