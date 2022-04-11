3519ad057f9355605588b124addcbec8dd17994c
Benjamin Osmers <u16068344@tuks.co.za>
2022-04-11 10:28:06 +0200

    ✨ (shorts-api): Fixed Imports

--- libs/api/shorts/service/feature/src/lib/ApiShortsService.service.ts
+++ libs/api/shorts/service/feature/src/lib/ApiShortsService.service.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import { QueryBus } from '@nestjs/cqrs';
-import { Injectable } from '@nestjs/common';
-import { GetAllShortsQuery } from './queries/ApiShortsFetchAllQuery.query';
-=======
->>>>>>> e1e6e938374a1c07b6591fd84ab678bb1da472d2
 import { Short } from '@graduates/api/shorts/api/shared/entities/data-access';
 import { Injectable } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
--- libs/api/shorts/service/feature/src/lib/queries/handlers/ApiShortsFetchAllHandler.handler.ts
+++ libs/api/shorts/service/feature/src/lib/queries/handlers/ApiShortsFetchAllHandler.handler.ts
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
-import { GetAllShortsQuery } from '../ApiShortsFetchAllQuery.query';
-=======
->>>>>>> e1e6e938374a1c07b6591fd84ab678bb1da472d2
 import { ShortsRepository } from '@graduates/api/shorts/repository/data-access';
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { GetAllShortsQuery } from '../impl/ApiShortsFetchAllQuery.query';
