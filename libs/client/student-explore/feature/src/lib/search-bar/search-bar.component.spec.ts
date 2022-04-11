7048cfb2d3669fecc96da6a10e3c6a7b8352ea09
mattreed01 <matthewreed0107@gmail.com>
2022-04-11 19:01:52 +0200

    💄 (student-explore): Created a search bar component

---
+++ libs/client/student-explore/feature/src/lib/search-bar/search-bar.component.spec.ts
@@ -0,0 +1,28 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchBarComponent } from './search-bar.component';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+describe('SearchBarComponent', () => {
+  let component: SearchBarComponent;
+  let fixture: ComponentFixture<SearchBarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SearchBarComponent ],
+      imports: [MatFormFieldModule]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchBarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ libs/client/student-explore/feature/src/lib/search-bar/search-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+  selector: 'graduates-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss'],
+  providers: [MatFormFieldModule]
+})
+export class SearchBarComponent {
+
+  constructor() { 
+    //Do Something
+  }
+}
