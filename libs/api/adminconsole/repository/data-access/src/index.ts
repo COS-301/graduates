8e33cae3ee6553dc62c9b046db7520ce9ece8912
KayleePosthumus <kayleeposthumus1@gmail.com>
2022-04-11 12:21:45 +0200

    (db): Added Getters for Users, Stories, Blogs

--- libs/api/adminconsole/repository/data-access/src/index.ts
+++ libs/api/adminconsole/repository/data-access/src/index.ts
@@ -1,1 +1,23 @@
 export * from './lib/api-adminconsole-repository-data-access.module';
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+// User
+export async function GetAllUsers(){
+    const allUsers = await prisma.user.findMany();
+    return allUsers;
+}
+
+// Story
+export async function GetAllStories(){
+    const allStories = await prisma.story.findMany();
+    return allStories;
+}
+
+// Blog
+export async function GetAllBlogs(){
+    const allBlogs = await prisma.blog.findMany();
+    return allBlogs;
+}
