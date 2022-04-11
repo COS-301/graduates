b12d82dec7f063f834e27604d1a0524d2c4d30dc
Chiara Goncalves <u20632429@tuks.co.za>
2022-04-11 18:10:57 +0200

    ✨ (ui):create CompanyProfile directory and layout

---
+++ libs/client/companyProfile/feature/src/lib/companyProfile.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CompanyProfileComponent } from './companyProfile.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [CompanyProfileComponent],
+})
+export class CompanyProfileModule {}
