11da741c754aa78d5bd5e0a1ecd487a3e253174d
Ruben-Brits <u20463163@tuks.co.za>
2022-04-11 20:18:40 +0200

    approuter and feature routes setup

--- libs/client/shorts/feature/src/lib/shorts-routing.module.ts
+++ libs/client/shorts/feature/src/lib/shorts-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FeatureComponent } from './feature.component';
+import { StoryExploreComponent } from './story-explore/story-explore.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: FeatureComponent
+    component: StoryExploreComponent
   }
 ];
 
@@ -13,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class FeatureRoutingModule { }
+export class ShortsRoutingModule { }
