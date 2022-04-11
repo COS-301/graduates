6d124a89b7fcfaf274d7ddc4edc7eb156b3ad644
fffrikkie <u20532581@tuks.co.za>
2022-04-11 16:34:49 +0200

    Changed file structure

---
+++ libs/client/notifications/feature/src/lib/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'graduates-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent {
+  constructor() {
+    //do something
+   }
+}
