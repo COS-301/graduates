9fb2852a07bc9a2b9287f61c7720c2647e9b8fb4
mattreed01 <matthewreed0107@gmail.com>
2022-04-11 20:29:31 +0200

    ✅ (student-explore): Added test for search bar

--- libs/client/student-explore/feature/src/lib/search-bar/search-bar.component.spec.ts
+++ libs/client/student-explore/feature/src/lib/search-bar/search-bar.component.spec.ts
@@ -2,7 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchBarComponent } from './search-bar.component';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
@@ -11,7 +16,7 @@ describe('SearchBarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ SearchBarComponent ],
-      imports: [MatFormFieldModule]
+      imports: [MatFormFieldModule, MatInputModule, BrowserModule, BrowserAnimationsModule, FormsModule]
     })
     .compileComponents();
   });
@@ -25,4 +30,44 @@ describe('SearchBarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+});
+
+describe('SearchBarComponent Input', () => {
+  //Variables
+  let component: SearchBarComponent;
+  let fixture: ComponentFixture<SearchBarComponent>;
+
+  //Before Eaches
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SearchBarComponent ],
+      imports: [MatFormFieldModule, MatInputModule, BrowserModule, BrowserAnimationsModule, FormsModule]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchBarComponent);
+    component = fixture.componentInstance;
+    //fixture.detectChanges();
+  });
+
+  //Tests
+  it('Should submit a student name', () => {
+    const hostElement = fixture.nativeElement;
+    const input: HTMLInputElement = hostElement.querySelector('#search');
+
+    fixture.detectChanges();
+
+    input.value = 'Matthew Reed';
+
+    fixture.detectChanges();
+
+    input.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+
+    expect(component.qry).toBe('Matthew Reed');
+  });
+
 });
--- libs/client/student-explore/feature/src/lib/search-bar/search-bar.component.ts
+++ libs/client/student-explore/feature/src/lib/search-bar/search-bar.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'graduates-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss'],
-  providers: [MatFormFieldModule]
+  providers: [MatFormFieldModule, MatInputModule, BrowserModule, BrowserAnimationsModule, FormsModule]
 })
 export class SearchBarComponent {
+  qry: string;
 
   constructor() { 
-    //Do Something
+    this.qry = '';
   }
 }
