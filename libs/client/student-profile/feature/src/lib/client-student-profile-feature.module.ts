c0c4675178e6cb45eb8533b0e5c9b7cdacc4ef16
caaaramia <u20478144@tuks.co.za>
2022-04-12 20:31:45 +0200

    💄Added request-for-access component into profile-body

--- libs/client/student-profile/feature/src/lib/client-student-profile-feature.module.ts
+++ libs/client/student-profile/feature/src/lib/client-student-profile-feature.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+//component imports
 import { ProfileBodyComponent } from './profile-body/profile-body.component';
+import { RequestForAccessModule } from '@graduates/client/request-for-access/feature';
 
 @NgModule({
   imports: [
     CommonModule,
-
+    RequestForAccessModule,
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
