03c047224e6840e3500e01caae6598acd1f4f5f9
Mohamed Nizhar Aboobaker <nizhar123mna@gmail.com>
2022-04-13 23:56:39 +0200

    💄 Added dropdown menus to student-profiles

--- libs/client/student-profile/feature/src/lib/client-student-profile-feature.module.ts
+++ libs/client/student-profile/feature/src/lib/client-student-profile-feature.module.ts
@@ -7,10 +7,15 @@ import { ProfileBodyComponent } from './profile-body/profile-body.component';
 import { RequestForAccessModule } from '@graduates/client/request-for-access/feature';
 import { UserInfoComponent } from './user-info/user-info.component';
 import { ProfileRoutingModule } from './profile-routing.module';
+import { TitleComponent } from './title/title.component';
+import { DegreeComponent } from './degree/degree.component';
+import { EmploymentStatusComponent } from './employment-status/employment-status.component';
+import { LocationComponent } from './location/location.component';
 
 // importing material 
 import { MatDividerModule } from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
+
 
 
 @NgModule({
@@ -26,7 +31,11 @@ import {MatListModule} from '@angular/material/list';
   ],
   declarations: [
     ProfileBodyComponent,
-    UserInfoComponent
+    UserInfoComponent,
+    TitleComponent,
+    DegreeComponent,
+    EmploymentStatusComponent,
+    LocationComponent
   ],
    exports: [ProfileBodyComponent]
 })
