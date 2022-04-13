b5c87be9a40ae9e3f80bf396e2c12c21fd030deb
David Roodt <davidroodt2@gmail.com>
2022-04-13 17:29:35 +0200

    Added sets

--- libs/api/student-profiles/repository/data-access/src/lib/api-student-profiles-repository-data-access.ts
+++ libs/api/student-profiles/repository/data-access/src/lib/api-student-profiles-repository-data-access.ts
@@ -1,4 +1,5 @@
-import {PrismaClient} from '@prisma/client';
+import {FileCategory, PrismaClient, SocialMedia} from '@prisma/client';
+//todo: david roodt: implement employment status once added to db
 
 export class StudentProfilesRepository
 {
@@ -14,6 +15,20 @@ export class StudentProfilesRepository
             select:
             {
                 name: true
+            }
+        });
+    }
+
+    async setName(userid : string, newname : string)
+    {
+        return await this.prisma.user.update({
+            data:
+            {
+                name: newname
+            },
+            where:
+            {
+                id: userid
             }
         });
     }
@@ -46,6 +61,20 @@ export class StudentProfilesRepository
         });
     }
 
+    async setPfp(userid : string, newPfp : string)
+    {
+        return await this.prisma.userProfile.update({
+            data:
+            {
+                profilePicture: newPfp
+            },
+            where:
+            {
+                userId: userid
+            }
+        });
+    }
+
     async getBio(userid : string)
     {        
         return await this.prisma.userProfile.findFirst({
@@ -60,6 +89,20 @@ export class StudentProfilesRepository
         });
     }
 
+    async setBio(userid : string, newbio : string)
+    {
+        return await this.prisma.userProfile.update({
+            data:
+            {
+                bio: newbio
+            },
+            where:
+            {
+                userId: userid
+            }
+        });
+    }
+
     async getTags(userid : string)
     {        
         return await this.prisma.userTag.findMany({
@@ -70,6 +113,27 @@ export class StudentProfilesRepository
             select:
             {
                 tag : true
+            }
+        });
+    }
+
+    async addTag(userid : string, newtag : string)
+    {
+        return await this.prisma.userTag.create({
+            data:
+            {
+                userId: userid,
+                tag: newtag
+            }
+        });
+    }
+
+    async removeTag(userid : string, tag : string)
+    {
+        return await this.prisma.userTag.delete({
+            where:
+            {
+                userId: userid
             }
         });
     }
@@ -89,6 +153,28 @@ export class StudentProfilesRepository
         });
     }
 
+    async addSocialMedia(userid : string, type: SocialMedia, link: string)
+    {        
+        return await this.prisma.userSocialMedia.create({
+            data:
+            {
+                userId: userid,
+                type: type,
+                link: link
+            }
+        });
+    }
+
+    async removeSocialMedia(userid : string)
+    {        
+        return await this.prisma.userSocialMedia.delete({
+            where:
+            {
+                userId: userid
+            }
+        });
+    }
+
     async getLocation(userid : string)
     {        
         return await this.prisma.userLocation.findFirst({
@@ -99,6 +185,20 @@ export class StudentProfilesRepository
             select:
             {
                 location : true
+            }
+        });
+    }
+
+    async setLocation(userid : string, location: string)
+    {        
+        return await this.prisma.userLocation.update({
+            data:
+            {
+                location: location
+            },
+            where:
+            {
+                userId : userid
             }
         });
     }
@@ -117,6 +217,20 @@ export class StudentProfilesRepository
         });
     }
 
+    async setEmails(userid : string, email: string)
+    {        
+        return await this.prisma.userEmail.update({
+            data:
+            {
+                email: email
+            },
+            where:
+            {
+                userId : userid
+            }
+        });
+    }
+
     async getFiles(userid : string)
     {        
         return await this.prisma.userProfileFile.findMany({
@@ -129,6 +243,29 @@ export class StudentProfilesRepository
                 fileCategory : true,
                 filePath : true,
                 fileExtension : true
+            }
+        });
+    }
+
+    async addFiles(userid : string, fileCategory : FileCategory, filePath : string, fileExtension : string)
+    {        
+        return await this.prisma.userProfileFile.create({
+            data:
+            {
+                userId: userid,
+                fileCategory : fileCategory,
+                filePath : fileCategory,
+                fileExtension : fileExtension
+            }
+        });
+    }
+
+    async removeFiles(userid : string, fileCategory : FileCategory)
+    {        
+        return await this.prisma.userProfileFile.delete({
+            where:
+            {
+                fileId: userid
             }
         });
     }
