f1229bc5a9fba7844132c05bcb1a90e9eb88d88e
Michael du Plessis <mdp0101@gmail.com>
2022-04-13 15:50:16 +0200

    :art: AccessStatusEntity no longer contains the graduates ID and added data type to api-access-service parameters and requestAccess paramter name changed

--- libs/api/access-status/api/feature/src/lib/api-access-status.entity.ts
+++ libs/api/access-status/api/feature/src/lib/api-access-status.entity.ts
@@ -2,9 +2,6 @@ import { Field, ID, ObjectType } from "@nestjs/graphql";
 
 @ObjectType()
 export class ApiAccessStatusEntity {
-    @Field(type => ID)
-    userID!: string;
-
     @Field()
     item!: string;
 
