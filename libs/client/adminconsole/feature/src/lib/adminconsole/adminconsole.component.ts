500e28dd2b694c8c7cfa56948d0eefdaf2d4e5ee
Bevan Slabbert <89096655+bevanslabbert@users.noreply.github.com>
2022-04-13 08:06:07 +0200

    reverted small changes

--- libs/client/adminconsole/feature/src/lib/adminconsole/adminconsole.component.ts
+++ libs/client/adminconsole/feature/src/lib/adminconsole/adminconsole.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'graduates-adminconsole',
+  selector: 'cards-adminconsole',
   templateUrl: './adminconsole.component.html',
   styleUrls: ['./adminconsole.component.scss']
 })
