82bf7ab7e73f85d59288e02c30dcc046d21aa73c
Agape <theaman249@gmail.com>
2022-04-13 07:47:35 +0200

    🚧 (services) Added QueryBus and CommandBus to api-company-representative.resolver.spec.ts as providers

--- libs/api/company-representative/api/feature/src/lib/api-company-representative.resolver.spec.ts
+++ libs/api/company-representative/api/feature/src/lib/api-company-representative.resolver.spec.ts
@@ -1,4 +1,5 @@
 import { ApiCompanyRepresentativeService } from '@graduates/api/company-representative/service/feature';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApiCompanyRepresentativeResolver } from './api-company-representative.resolver';
 
@@ -7,7 +8,7 @@ describe('ApiCompanyRepresentativeResolver', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ApiCompanyRepresentativeResolver, ApiCompanyRepresentativeService],
+      providers: [ApiCompanyRepresentativeResolver, ApiCompanyRepresentativeService,CommandBus,QueryBus],
     }).compile();
 
     resolver = module.get<ApiCompanyRepresentativeResolver>(ApiCompanyRepresentativeResolver);
