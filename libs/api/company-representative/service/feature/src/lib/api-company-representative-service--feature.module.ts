0c724d37394ff70f97ec560b44f2488d6f4b6a86
Agape <theaman249@gmail.com>
2022-04-13 02:07:36 +0200

    💚 (repository/Services): Made QueryBus a provider in module.ts

--- libs/api/company-representative/service/feature/src/lib/api-company-representative-service--feature.module.ts
+++ libs/api/company-representative/service/feature/src/lib/api-company-representative-service--feature.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ApiCompanyRepresentativeService } from './api-company-representative.service';
-import {CqrsModule} from '@nestjs/cqrs'
+import {CqrsModule, QueryBus} from '@nestjs/cqrs'
 
 
 import { QueryHandlers } from './queries/handlers';
@@ -11,7 +11,7 @@ import { DataAccess } from '@graduates/api/company-representative/repository';
 @Module({
   imports:[CqrsModule],
   controllers: [],
-  providers: [ApiCompanyRepresentativeService,DataAccess,...QueryHandlers],
+  providers: [ApiCompanyRepresentativeService,DataAccess,...QueryHandlers,QueryBus],
   exports: [ApiCompanyRepresentativeService],
 })
 export class ApiCompanyRepresentativeServiceFeatureModule {}
