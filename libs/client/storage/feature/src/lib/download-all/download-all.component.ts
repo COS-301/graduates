10f4517d0ab5dc713c4afdde557e9a9a4a451640
GO-Shoderu <go.shoderu@gmail.com>
2022-04-14 00:25:34 +0200

    📝 (Storage/UI): Making sure all imported components are visible on the main page

---
+++ libs/client/storage/feature/src/lib/download-all/download-all.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'graduates-download-all',
+  templateUrl: './download-all.component.html',
+  styleUrls: ['./download-all.component.scss']
+})
+export class DownloadAllComponent implements OnInit {
+
+  constructor() {
+    // do something
+   }
+
+  ngOnInit(): void {
+    return
+  }
+
+}
