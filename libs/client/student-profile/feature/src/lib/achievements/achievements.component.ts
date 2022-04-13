57f47a5502fa91e25a7e49842cd994b12493ea31
caaaramia <u20478144@tuks.co.za>
2022-04-14 00:33:55 +0200

    💄 Created achievements compononent

---
+++ libs/client/student-profile/feature/src/lib/achievements/achievements.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'graduates-achievements',
+  templateUrl: './achievements.component.html',
+  styleUrls: ['./achievements.component.sass']
+})
+export class AchievementsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
