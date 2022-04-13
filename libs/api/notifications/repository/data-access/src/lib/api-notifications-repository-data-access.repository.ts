0cc45308646715566ddcc062b45a9260b7780835
Richard_20430168 <u20430168@tuks.co.za>
2022-04-13 16:08:15 +0200

    :construction: Begain implementation of api/repositoy/repository.ts (data-access functions)

--- libs/api/notifications/repository/data-access/src/lib/api-notifications-repository-data-access.repository.ts
+++ libs/api/notifications/repository/data-access/src/lib/api-notifications-repository-data-access.repository.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@graduates/api/shared/services/prisma/data-access';
+import { Notification, Prisma } from '@prisma/client';
 
 @Injectable()
 export class NotificationsRepository {
@@ -8,4 +9,41 @@ export class NotificationsRepository {
   async findAll() {
     return this.prisma.notification.findMany();
   }
+
+  async findById(id: string): Promise<Notification | null> {
+    return this.prisma.notification.findUnique({ 
+      where: { 
+        id : id
+      }
+    });
+  }
+
+  async findbyUserIDTo(userId : string): Promise<Notification[] | null> {
+    return this.prisma.notification.findMany({
+      where: {
+        userIdTo : userId
+      }
+    });
+  }
+
+  async findByNotificationType(userId : string, notificationType : string): Promise<Notification[] | null> {
+
+    const notifications =  this.prisma.notification.findMany({ 
+      where: {
+        userIdTo : userId,
+      }
+    });
+
+    // (await notifications).forEach(notification => {
+    //   if (notification.data && typeof notification.data === 'object' && !Array.isArray(notification.data)) {
+    //     const notificationData = notification.data as Prisma.JsonObject
+    //     if (notificationData['notificationType'] === notificationType) {
+
+    //     }
+    //   }
+    // })
+    
+    return notifications;
+  }
+
 }
