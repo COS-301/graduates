9dd5d8c3a120e4aa59edcb01f31e73916f465a53
GO-Shoderu <go.shoderu@gmail.com>
2022-04-13 10:34:22 +0200

    📝 (Storage/UI): Added responsiveness to the first Storage page

---
+++ libs/client/storage/feature/src/lib/client-storage-feature.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Importing all the component that will be used
+import { StorageFeatureComponent } from './client-storage-feature.component';
+import { PageTitleComponent } from './page-title/page-title.component';
+import { DeleteAllComponent } from './delete-all/delete-all.component';
+import { DownloadAllComponent } from './download-all/download-all.component';
+// import { StorageFeatureRoutingModule } from './client-storage-feature-routing.module';
+
+// importing material things
+import { MatGridListModule } from '@angular/material/grid-list';
+
+@NgModule({
+  declarations: [
+    StorageFeatureComponent, 
+    PageTitleComponent, 
+    DeleteAllComponent, 
+    DownloadAllComponent
+  ],
+  imports: [
+    CommonModule, 
+    MatGridListModule
+  ],
+  exports: [StorageFeatureComponent],
+})
+export class StorageFeatureModule {}
+
+// client-storage-feature-routing.module.ts
