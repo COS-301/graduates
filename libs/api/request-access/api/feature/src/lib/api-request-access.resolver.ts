fb9d37ccc77febbb3896542a38554e94dae736e5
Michael du Plessis <mdp0101@gmail.com>
2022-04-14 13:32:28 +0200

    :recycle: Changed array of possible items to request for to an enum

--- libs/api/request-access/api/feature/src/lib/api-request-access.resolver.ts
+++ libs/api/request-access/api/feature/src/lib/api-request-access.resolver.ts
@@ -9,11 +9,19 @@ export class ApiRequestAccessResolver {
     @Mutation(returns => ApiRequestAccessEntity, { nullable: true })
     // must add paramters to request
     async requestAccess(@Args('compId', { type: () => ID }) compId: string, @Args('gradId', { type: () => ID }) gradId: string, @Args('item') item: string): Promise<ApiRequestAccessEntity> {
+        enum Items {
+            CV = "CV",
+            Transcript = "Transcript",
+            Academic = "Academic",
+            Certificates = "Certificates",
+            Capstone = "Capstone"
+        };
+
         if (compId == "" || gradId == "" || item == "") // obviously empty elements are not allowed
             return null;
 
         // if new items become available add here
-        if (!["CV", "Transcript", "Academic", "Certificates", "Capstone"].includes(item)) // if a valid item has been requested
+        if (!(item in Items)) // if a valid item has been requested
             return null;
 
         return this.requestAccessService.makeRequest();
