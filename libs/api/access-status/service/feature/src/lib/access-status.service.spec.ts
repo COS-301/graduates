aa63240641f4e3adad7de8be71532b6bcf28060f
Farai <farai.chivunga.112@gmail.com>
2022-04-14 17:50:42 +0200

    ✅ (Testing): Add Unit Tests for Access Status

--- libs/api/access-status/service/feature/src/lib/access-status.service.spec.ts
+++ libs/api/access-status/service/feature/src/lib/access-status.service.spec.ts
@@ -15,4 +15,23 @@ describe('AccessStatusService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should be not be null',async () => {
+    const call = await service.getAccessStatus("u12345678","42");
+    console.log(call);
+    expect(call).not.toBeNull();
+  });
+
+  it('should be equal to entity',async () => {
+    const call = await service.getAccessStatus("u12345678","42");
+    console.log(call);
+    expect(call).toEqual([{"accessStatus": "Pending", "item": "CV"}]);
+  });  
+
+  it('should be equal to entity',async () => {
+    const call = await service.getAccessStatus("u12345678","5");
+    console.log(call);
+    expect(call).toEqual([{"accessStatus": "Rejected", "item": "Academic Record"}]);
+  });
+
 });
