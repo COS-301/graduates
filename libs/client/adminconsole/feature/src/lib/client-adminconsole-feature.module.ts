8802b5df2947a0d3a6d8d613991a2b96d69fa6f5
Bevan Slabbert <89096655+bevanslabbert@users.noreply.github.com>
2022-04-14 08:30:58 +0200

    Imported header and footer to adminconsle feature

--- libs/client/adminconsole/feature/src/lib/client-adminconsole-feature.module.ts
+++ libs/client/adminconsole/feature/src/lib/client-adminconsole-feature.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminconsoleComponent } from './adminconsole/adminconsole.component';
+import { HeaderComponent } from '../../../../shared/components/header-and-footer/ui/header-and-footer/src/app/header/header.component';
+import { FooterComponent } from '../../../../shared/components/header-and-footer/ui/header-and-footer/src/app/footer/footer.component';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
-    AdminconsoleComponent
+    AdminconsoleComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   exports: [
     AdminconsoleComponent
