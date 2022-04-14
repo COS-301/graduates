63a44b7e42c10a465eaabdbe40f6d0f82df67c14
Maduna Thabo <u19116498@tuks.co.za>
2022-04-14 19:37:14 +0200

    🚑 (Noifications): fix handlers

--- libs/api/notifications/service/feature/src/lib/events/handlers/send-mail.handler.ts
+++ libs/api/notifications/service/feature/src/lib/events/handlers/send-mail.handler.ts
@@ -1,37 +1,37 @@
 import { EventsHandler, IEventHandler } from "@nestjs/cqrs";
 import { SendMailEvent } from "../impl/send-mail.event";
 import * as nodemailer from 'nodemailer';
-// import { ApiNotificationsRepositoryDataAccessModule } from "@graduates/api/notifications/repository/data-access";
+import { NotificationsRepository } from "@graduates/api/notifications/repository/data-access";
 
 @EventsHandler(SendMailHandler)
 export class SendMailHandler implements IEventHandler<SendMailEvent>{
-    // constructor(private repository: ApiNotificationsRepositoryDataAccessModule) {}
+    constructor(public repository: NotificationsRepository) {}
 
     handle(event: SendMailEvent) {
-        // const {rep} = this.repository.findAll() when the API is fully functional
-
         // const notification = this.repository.findByUserIdTo("cl1rpkemf0148e7x5zy7087ma")
+        const notification = this.repository.findNotificationsAll()
 
-        // const transport = nodemailer.createTransport({
-        //     service: 'gmail',
-        //     auth: {
-        //         user: "upminiproject301@gmail.com",
-        //         pass: "miniproject301"
-        //     }
-        //     });
+        const transport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: "upminiproject301@gmail.com",
+                pass: "miniproject301"
+            }
+        });
         
-        // const message = {
-        //     from: "upminiproject301@gmail.com",
-        //     to: notification.email,   //data.email    
-        //     subject: notification.subject,         //data.notification.data
-        //     text: notification.data
-        // }
-        // transport.sendMail(message, function(err, info) {
-        //     if (err) {
-        //         console.log(err);
-        //     } else {
-        //         console.log(info);
-        //     }
-        // })
+        const message = {
+            from: "upminiproject301@gmail.com",
+            to: "u20430168@tuks.co.za",   //data.email    
+            subject: notification[0].subject,         //data.notification.data
+            text: notification[0].data
+        }
+        
+        transport.sendMail(message, function(err, info) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(info);
+            }
+        })
     }
 }
--- libs/api/notifications/service/feature/src/lib/queries/handlers/get-all-user-notifications.handlers.ts
+++ libs/api/notifications/service/feature/src/lib/queries/handlers/get-all-user-notifications.handlers.ts
@@ -1,16 +1,16 @@
 
-// import { NotificationsRepository } from '@graduates/api/shorts/repository/data-access';
-import { ApiNotificationsRepositoryDataAccessModule } from '@graduates/api/notifications/repository/data-access';
+import { NotificationsRepository } from "@graduates/api/notifications/repository/data-access";
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { GetAllUserNotificationsQuery } from '../impl/get-all-user-notifications.query';
-import { ApiNotificationsService } from '../../api-notifications-service-feature.service';
+// import { ApiNotificationsService } from '../../api-notifications-service-feature.service';
+
 
 @QueryHandler(GetAllUserNotificationsQuery)
 export class GetAllUserNotificationsHandler implements IQueryHandler<GetAllUserNotificationsQuery> {
-  constructor(private readonly repository: ApiNotificationsRepositoryDataAccessModule) {}
+  constructor(private readonly repository: NotificationsRepository) {}
 
   async execute() {
-    // return this.repository.findAll();
+    return this.repository.findNotificationsAll();
   }
 }
 
