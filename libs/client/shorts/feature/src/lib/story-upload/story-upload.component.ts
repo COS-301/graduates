f67ca3897cfa0f72829c26517fa0c6a866494a9f
Dylan Whitefod <u19214597@tuks.co.za>
2022-04-14 18:03:26 +0200

    removed imports

--- libs/client/shorts/feature/src/lib/story-upload/story-upload.component.ts
+++ libs/client/shorts/feature/src/lib/story-upload/story-upload.component.ts
@@ -1,17 +1,4 @@
 import { Component } from '@angular/core';
-
-
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-
 @Component({
   selector: 'graduates-story-upload',
   templateUrl: './story-upload.component.html',
