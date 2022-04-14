0f90d6fdcba9b2eb610f493da5c89d9ebde85acc
TimovdMerwe-18 <u19048280@tuks.co.za>
2022-04-14 15:40:40 +0200

    Fixed changes made during testing 2

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { ApiShellFeatureModule } from '@graduates/api/shell/feature';
 import { Module } from '@nestjs/common';
 
-
 @Module({
-  
+  imports: [ApiShellFeatureModule],
 })
 export class AppModule {}
