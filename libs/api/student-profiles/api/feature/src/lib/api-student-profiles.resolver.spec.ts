431003184b0951a4a3593ae2b24e1616edcaa424
Tabby709 <u19294418@tuks.co.za>
2022-04-14 20:13:55 +0200

    👔 (service) : fixed a test

--- libs/api/student-profiles/api/feature/src/lib/api-student-profiles.resolver.spec.ts
+++ libs/api/student-profiles/api/feature/src/lib/api-student-profiles.resolver.spec.ts
@@ -2,12 +2,13 @@ import { ApiStudentProfileService } from '@graduates/api/student-profiles/servic
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApiStudentProfileResolver } from './api-student-profiles.resolver';
 import { CommandBus } from '@nestjs/cqrs';
+
 describe('ApiStudentProfileResolver', () => {
   let resolver: ApiStudentProfileResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ApiStudentProfileResolver,ApiStudentProfileService],
+      providers: [ApiStudentProfileResolver,ApiStudentProfileService,CommandBus],
     }).compile();
 
     resolver = module.get<ApiStudentProfileResolver>(ApiStudentProfileResolver);
