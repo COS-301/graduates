99cc0f0d2e33eea5a3a7b654c2094352d202fc55
GO-Shoderu <go.shoderu@gmail.com>
2022-04-14 14:59:50 +0200

    📝 (Storage/UI): Added all the buttons required

--- libs/client/storage/feature/src/lib/file-upload/file-upload.component.ts
+++ libs/client/storage/feature/src/lib/file-upload/file-upload.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileUploadComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    // do something
+   }
 
   ngOnInit(): void {
+    return
   }
 
 }
