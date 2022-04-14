94776cca56aa65721875ed3d57e7a53c32576267
Joshua Young <u20442018@up.ac.za>
2022-04-14 17:34:59 +0200

    🎨 (repository): Add adminconsole to shell feature

--- libs/api/adminconsole/service/feature/src/lib/api-adminconsole-service-feature.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ApiAdminconsoleServiceFeatureService } from './api-adminconsole-service-feature.service';
-
-@Module({
-  controllers: [],
-  providers: [ApiAdminconsoleServiceFeatureService],
-  exports: [ApiAdminconsoleServiceFeatureService],
-})
-export class ApiAdminconsoleServiceFeatureModule {}
