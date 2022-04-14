58ded9124fe16c8f23fdadd3d2bf2ecc6fa358b9
TimovdMerwe-18 <u19048280@tuks.co.za>
2022-04-14 15:53:59 +0200

    Fixed changes made during testing 6

--- libs/client/student-profile/feature/src/lib/client-student-profile-feature.module.ts
+++ libs/client/student-profile/feature/src/lib/client-student-profile-feature.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 //component imports
 import { ProfileBodyComponent } from './profile-body/profile-body.component';
