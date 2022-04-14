4d3881c881e697b852f90a49b2cc3187edba89a0
Joshua Young <u20442018@up.ac.za>
2022-04-14 18:56:28 +0200

    🎨 (repository): Add adminconsole to shell feature

--- libs/api/adminconsole/api/feature/src/lib/api-adminconsole.resolver.spec.ts
+++ libs/api/adminconsole/api/feature/src/lib/api-adminconsole.resolver.spec.ts
@@ -1,15 +1,17 @@
-import { TestBed } from '@angular/core/testing';
-
+import { ApiAdminConsoleServiceFeatureModule } from '@graduates/api/adminconsole/service/feature';
+import { Test, TestingModule } from '@nestjs/testing';
 import { ApiAdminConsoleResolver } from './api-adminconsole.resolver';
 
 describe('ApiAdminConsoleResolver', () => {
   let resolver: ApiAdminConsoleResolver;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    resolver = TestBed.inject(ApiAdminConsoleResolver);
-  });
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ApiAdminConsoleResolver, ApiAdminConsoleServiceFeatureModule],
+    }).compile();
 
+    resolver = module.get<ApiAdminConsoleResolver>(ApiAdminConsoleResolver);
+  });
   it('should be created', () => {
     expect(resolver).toBeTruthy();
   });
