2ebfc5fc89003a127b5b9665718076a8802f13b1
Grobbies26 <74908721+Grobbies26@users.noreply.github.com>
2022-04-15 16:46:12 +0200

    ✨ (up-integration): added .spec file for the entitiy file

---
+++ libs/api/upintegration/api/shared/data-access/src/lib/api-upintegration-studentdetails.spec.ts
@@ -0,0 +1,7 @@
+import { StudentDetails } from './api-upintegration-studentdetails.entity';
+
+describe('StudentDetails', () => {
+  it('should be defined', () => {
+    expect(new StudentDetails()).toBeDefined();
+  });
+});
