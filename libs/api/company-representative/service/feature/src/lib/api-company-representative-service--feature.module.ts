6dfcc2dda2c5c8f369a668e5579f350c0c97c7ee
Agape <theaman249@gmail.com>
2022-04-15 11:51:58 +0200

    🚧 (services):Added a createUser function

--- libs/api/company-representative/service/feature/src/lib/api-company-representative-service--feature.module.ts
+++ libs/api/company-representative/service/feature/src/lib/api-company-representative-service--feature.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { CqrsModule } from '@nestjs/cqrs';
+import { CommandBus, CqrsModule, QueryBus } from '@nestjs/cqrs';
 import { ApiCompanyRepresentativeService } from './api-company-representative.service';
 
 import { GetAllRepresentativesHandler, GetOneRepresentativeHandler } from './queries/handlers';
+import { CreateCompanyRepresentativeHandler } from './commands/handlers';
 
 @Module({
   controllers: [CqrsModule],
-  providers: [ApiCompanyRepresentativeService,GetAllRepresentativesHandler,GetOneRepresentativeHandler],
+  providers: [ApiCompanyRepresentativeService,GetAllRepresentativesHandler,GetOneRepresentativeHandler,CreateCompanyRepresentativeHandler,QueryBus,CommandBus],
   exports: [ApiCompanyRepresentativeService],
 })
 export class ApiCompanyRepresentativeServiceFeatureModule {}
