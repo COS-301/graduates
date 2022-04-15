d4e3fa41b84d7e1e9bd093fc546fe7727ab32f66
Sean Nkosi <seann@agilebridge.co.za>
2022-04-15 16:26:57 +0200

    Added routing test import

--- libs/client/company-representative/feature/src/lib/company-representative-mock-login-page/company-representative-mock-login-page.component.spec.ts
+++ libs/client/company-representative/feature/src/lib/company-representative-mock-login-page/company-representative-mock-login-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { CompanyRepresentativeMockLoginPageComponent } from './company-representative-mock-login-page.component';
 
 describe('CompanyRepresentativeMockLoginPageComponent', () => {
@@ -8,6 +8,7 @@ describe('CompanyRepresentativeMockLoginPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ CompanyRepresentativeMockLoginPageComponent ]
     })
     .compileComponents();
