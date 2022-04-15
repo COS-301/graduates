b6a8c72a78691d612b80141d02fa9c892bd005b8
MoeMoola3 <73942516+MoeMoola3@users.noreply.github.com>
2022-04-16 01:40:55 +0200

    🚧

--- libs/api/authentication/service/feature/src/lib/users.service.ts
+++ libs/api/authentication/service/feature/src/lib/users.service.ts
@@ -1,20 +1,20 @@
 import { Injectable, NotFoundException  } from '@nestjs/common';
-import { User } from '@graduates/api/authentication/api/shared/interfaces/data-access';
+// import { User } from '@graduates/api/authentication/api/shared/interfaces/data-access';
 import { RegisterCommand } from './commands/RgisterCommand';
 import { LoginQuery } from './queries/LoginQuery';
 
 @Injectable()
 export class UsersService {
-    async findAll(): Promise<User[]>{
-        const user = new User();
-        user.id = '1';
-        user.name = 'John';
-        user.email = 'JohnDoe@gmail.com';
+    // async findAll(): Promise<User[]>{
+    //     const user = new User();
+    //     user.id = '1';
+    //     user.name = 'John';
+    //     user.email = 'JohnDoe@gmail.com';
     
 
-        return [user];
+    //     return [user];
 
-    }
+    // }
 
       /*constructor(
         private readonly commandBus: CommandBus,
