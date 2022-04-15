b867dbc52b9981dac24ec46290e82a9f7781256f
Kearon Foba <23222438+KearonF@users.noreply.github.com>
2022-04-15 16:20:31 +0200

    ✅ (Shorts-DB): added some integration tests (WIP)

--- libs/api/shorts/repository/data-access/src/lib/api-shorts-repository.repository.spec.ts
+++ libs/api/shorts/repository/data-access/src/lib/api-shorts-repository.repository.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ShortsRepository } from './api-shorts-repository.repository';
 import { PrismaService } from '@graduates/api/shared/services/prisma/data-access';
-import { Short } from '@graduates/api/shorts/api/shared/entities/data-access';
+import { Short, ShortTag } from '@graduates/api/shorts/api/shared/entities/data-access';
+import { User } from '@graduates/api/authentication/api/shared/interfaces/data-access';
 
 jest.mock('@graduates/api/shorts/api/shared/entities/data-access');
 
 const shortMock: jest.Mocked<Short> = new Short() as Short;
+const shortTagMock: jest.Mocked<ShortTag> = new ShortTag() as ShortTag;
 
 // Run `yarn test api-shorts-repository-data-access`
 describe('ShortsRepository', () => {
@@ -92,4 +94,133 @@ describe('ShortsRepository', () => {
       );
     });
   });
+});
+
+//! TODO Uncomment once DB and API are ready in CI/CD or Deployment Environment
+describe("DB Integration Tests", () => {
+  let repository: ShortsRepository;
+  let prisma: PrismaService;  
+  let shortTest: Short;
+  let shortTagTest: ShortTag;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ShortsRepository, PrismaService],
+    }).compile();
+
+    prisma = module.get<PrismaService>(PrismaService);
+    repository = module.get<ShortsRepository>(ShortsRepository);
+  });
+
+  it('should be defined', () => {
+    expect(prisma).toBeDefined();
+    expect(repository).toBeDefined();
+  });
+  
+  describe("findAll", () => {
+    it("should return an array", async () => {
+      const result = await repository.findAll();
+      if (result.length > 0) {
+        expect(result).toMatchObject([shortMock]);
+      }
+      else {
+        expect(result).toMatchObject([]);
+      }
+    });
+  });
+
+  describe('findAllShortsPaged', () => {
+    it('should return an array', async () => {
+      const result = await repository.findAllShortsPaged(1, 10);
+      if (result.length > 0) {
+        expect(result).toMatchObject([shortTest]);
+      }
+      else {
+        expect(result).toMatchObject([]);
+      }
+    });
+  });
+
+  describe("findUserById", () => {
+    it("should return a user or null", async () => {
+      const result = await repository.findUserById("1");
+      if (result !== null) {
+        expect(result).toMatchObject(new User());
+      }
+      else {
+        expect(result).toBeNull();
+      }
+    });
+  });
+
+  describe('findByUser', () => {
+
+    it('should return an array', async () => {
+      const result = await repository.findByUser('1');
+      if (result.length > 0) {
+        expect(result).toMatchObject([shortTest]);
+      }
+      else {
+        expect(result).toMatchObject([]);
+      }
+    });
+  });
+
+  describe('findByTag', () => {
+      it('should return array', async () => {
+        const result = await repository.findByTag('1');
+        if (result.length > 0) {
+          expect(result).toMatchObject([shortTest]);
+        }
+        else {
+          expect(result).toMatchObject([]);
+        }
+      });
+    });
+
+    describe('findByTagPaged', () => {
+      it('should return array', async () => {
+        const result = await repository.findByTagPaged('1', 1, 10);
+        if (result.length > 0) {
+          expect(result).toMatchObject([shortMock]);
+        }
+        else {
+          expect(result).toMatchObject([]);
+        }
+      });
+    });
+
+    //! Cannot live test createTag as it requires a user to be created and there is no createUser function available, User table needs to be populated first
+    //! Cannot live test createShort as it requires a user to be created and there is no createUser function available, User table needs to be populated first
+    //! Cannot live test updateShort as it requires a user to be created and there is no createUser function available, User table needs to be populated first
+    //! Cannot live test deleteShort as it requires a user to be created and there is no createUser function available, User table needs to be populated first
+    //! Cannot live test deleteShortReport as it requires a user to be created and there is no createUser function available, User table needs to be populated first
+
+    describe("findAllTags", () => {
+      it("should return an array", async () => {
+        const result = await repository.findAllTags();
+        if (result.length > 0) {
+          expect(result).toMatchObject([shortTagTest]);
+        }
+        else {
+          expect(result).toMatchObject([]);
+        }
+      });
+    });
+
+    describe("findTagByShortId", () => {
+      it("should return an array", async () => {
+        const result = await repository.findTagByShortId("1");
+        if (result.length > 0) {
+          expect(result).toMatchObject([shortTagTest]);
+        }
+        else {
+          expect(result).toMatchObject([]);
+        }
+      });
+    });
+
+    
+
+
 });
