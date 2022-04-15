4aad4fbd9acc9fd4518788db78ba1432242586f5
Grobbies26 <74908721+Grobbies26@users.noreply.github.com>
2022-04-15 17:01:51 +0200

    ✅ (up-integration): readded test-setup.ts, updated entity .spec file

--- libs/api/upintegration/api/shared/data-access/src/lib/api-upintegration-studentdetails.spec.ts
+++ libs/api/upintegration/api/shared/data-access/src/lib/api-upintegration-studentdetails.spec.ts
@@ -1,6 +1,8 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { StudentDetails } from './api-upintegration-studentdetails.entity';
 
 describe('StudentDetails', () => {
+
   it('should be defined', () => {
     expect(new StudentDetails()).toBeDefined();
   });
