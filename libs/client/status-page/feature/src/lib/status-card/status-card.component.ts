3e25f83a4c620e215b73ee95786b36bca67f404b
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2022-04-16 22:43:44 +0200

    Connected client to api

--- libs/client/status-page/feature/src/lib/status-card/status-card.component.ts
+++ libs/client/status-page/feature/src/lib/status-card/status-card.component.ts
@@ -1,10 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'graduates-status-card',
   templateUrl: './status-card.component.html',
-  styleUrls: ['./status-card.component.scss']
+  styleUrls: ['./status-card.component.scss'],
 })
-export class StatusCardComponent {
-  @Input() image = "";
+export class StatusCardComponent implements OnInit {
+  operationalImage =
+    'https://icon-library.com/images/tick-icon-png/tick-icon-png-17.jpg';
+  nonOperationalImage =
+    'https://icon-library.com/images/red-cross-icon-png/red-cross-icon-png-4.jpg';
+  underDevelopmentImage =
+    'https://icon-library.com/images/warning-icon-svg/warning-icon-svg-18.jpg';
+
+  image: string = '';
+
+  @Input() status: any;
+
+  ngOnInit(): void {
+    if (this.status.status == 'Operational') {
+      this.image = this.operationalImage;
+    } else if (this.status.status == 'Under Development') {
+      this.image = this.underDevelopmentImage;
+    } else if (this.status.status == 'Non Operational') {
+      this.image = this.nonOperationalImage;
+    }
+  }
 }
