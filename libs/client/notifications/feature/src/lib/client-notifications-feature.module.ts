f359671ea3bee6c75f3bd2a3923c33b37dd4529d
fffrikkie <u20532581@tuks.co.za>
2022-04-16 13:57:25 +0200

    Fixed conflict

--- libs/client/notifications/feature/src/lib/client-notifications-feature.module.ts
+++ libs/client/notifications/feature/src/lib/client-notifications-feature.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from '../../../../shared/components/header-and-footer/ui/header-and-footer/src/app/footer/footer.component';
-import { HeaderComponent } from '../../../../shared/components/header-and-footer/ui/header-and-footer/src/app/header/header.component';
+import { HeaderModule } from '../../../../shared/components/header/src/lib/header.module';
+import { FooterModule } from '../../../../shared/components/footer/src/lib/footer.module';
 import { NotifDisplayComponent } from './notif-display/notif-display.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -17,17 +17,15 @@ import { ExpansionNotifComponent } from './expansion-notif/expansion-notif.compo
     MatCardModule,
     MatIconModule,
     MatExpansionModule,
-    MatListModule
+    MatListModule,
+    FooterModule,
+    HeaderModule
   ],
   declarations: [
-    FooterComponent,
-    HeaderComponent,
     NotifDisplayComponent,
     ExpansionNotifComponent
   ],
   exports: [
-    FooterComponent,
-    HeaderComponent,
     NotifDisplayComponent,
     ExpansionNotifComponent
   ],
