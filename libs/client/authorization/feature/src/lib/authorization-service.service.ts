b3cb5e2a4f279ebb64963dff3454bcd6a9b2cc11
GiftMonwa <84226371+GiftMonwa@users.noreply.github.com>
2022-04-16 08:43:48 +0200

    Added a cookie service

--- libs/client/authorization/feature/src/lib/authorization-service.service.ts
+++ libs/client/authorization/feature/src/lib/authorization-service.service.ts
@@ -4,59 +4,16 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthorizationServiceService {
   perm:any;
-
+  id="45543";  /* Going to be replaced with the userId extracted from a cookie*/
   constructor(private httpClient:HttpClient) {
 
-    this.httpClient.get("http://localhost")
-    this.httpClient.get("http://localhost:3000/User").subscribe(data=>{   //Subscribing to the observable that is returned by http client
+    this.httpClient.post("http://localhost:3333/graphql",{query:'query { authorization(${id}) { userId, userRole } }'}).subscribe(data=>{   //Subscribing to the observable that is returned by http client
       this.perm=data;
     });
    }
-
-
-  /*checkPermission(permission:string):boolean
-  {
-    if(this.perm.userRole==="unknown")
-    {
-      console.log("You are an Uknown User");
-      //this.displayError();
-      //Display the PopUp message
-      return false;
-    }
-    else
-    {
-      if(permission=="edit")
-      {
-        //Take User to the student Profile
-        if(this.perm.isUserPermitted.Edit==="true")
-        {
-          return true;
-        }
-      }
-      else if(permission=="Access")
-      {
-        if(this.perm.isUserPermitted.Access==="true")
-        {
-          return true;
-        }
-      }
-      else if(permission=="Delete")
-      {
-        if(this.perm.isUserPermitted.Delete==="true")
-        {
-          return true;
-        }
-      }
-      else
-      {
-        console.log("You have no permissions here");
-        return false;
-      }
-    }
-    return false;
-  }*/
 
   getRole():string
   {
