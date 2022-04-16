5e7c04b40fe5656fd550dba83121eecc32c4dfc7
Tabby709 <u19294418@tuks.co.za>
2022-04-16 21:01:35 +0200

    👔(service): updated business logic

--- libs/api/student-profiles/api/feature/src/lib/api-student-profiles.resolver.ts
+++ libs/api/student-profiles/api/feature/src/lib/api-student-profiles.resolver.ts
@@ -2,6 +2,8 @@ import { Query, Args, Resolver, Mutation } from '@nestjs/graphql';
 import { ApiStudentProfilesEntity } from '@graduates/api/student-profiles/api/shared/data-access';
 import { ApiStudentProfileService } from '@graduates/api/student-profiles/service/feature';
 import { ApiStudentProfilesInputEntity as StudentInput } from '@graduates/api/student-profiles/api/shared/data-access';
+import { stringify } from 'querystring';
+import { GetStudentProfilePhoneNumberQuery } from 'libs/api/student-profiles/service/feature/src/lib/queries/impl';
 
 @Resolver((of) => ApiStudentProfilesEntity)
 export class ApiStudentProfileResolver {
@@ -9,18 +11,38 @@ export class ApiStudentProfileResolver {
 
   @Query((returns) => ApiStudentProfilesEntity, { name: 'student' })
   async getStudent(@Args('studentNum', { type: () => String }) id: string) {
-  
+   // const studentArr = this.studentService.findById(id);
+    const studentObj = new ApiStudentProfilesEntity();
+   // studentObj.dateOfBirth = (await studentArr).pop();
+   // studentObj.phoneNum = (await studentArr).pop();
+    //studentObj.email = (await studentArr).pop();
+   // studentObj.firstName = (await studentArr).pop();
+    //studentObj.studentNum = (await studentArr).pop();
+    //studentObj.lastName = (await studentArr).pop();
+
+    return studentObj;
   }
 
   @Mutation((returns) => ApiStudentProfilesEntity)
   async editStudent(
     @Args('editStudentData') editStudentData: StudentInput
   ) {
-  
+    //const studentArr = this.studentService.update(editStudentData);
+    const studentObj = new ApiStudentProfilesEntity();
+   // studentObj.dateOfBirth = (await studentArr).pop();
+    //studentObj.phoneNum = (await studentArr).pop();
+    //studentObj.email = (await studentArr).pop();
+    //studentObj.firstName = (await studentArr).pop();
+    //studentObj.studentNum = (await studentArr).pop();
+    //studentObj.lastName = (await studentArr).pop();
+
+    return studentObj;
   }
 
   @Mutation((returns) => String)
   async deleteStudent(@Args('studentNum', {type: () => String})id: string ) {
-    
+    //const res = this.studentService.delete(id);
+    //return res;
+    return "test";
   }
 }
