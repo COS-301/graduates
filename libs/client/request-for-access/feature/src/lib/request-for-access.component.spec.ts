711e402ca3a5e9e2129e65936e09b13d3acf9d97
dylan2894 <61905387+dylan2894@users.noreply.github.com>
2022-04-16 23:48:32 +0200

    ✅(ui): Adding unit tests for request for access

--- libs/client/request-for-access/feature/src/lib/request-for-access.component.spec.ts
+++ libs/client/request-for-access/feature/src/lib/request-for-access.component.spec.ts
@@ -1,16 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDividerModule } from '@angular/material/divider';
+import { NgxsModule, Store } from '@ngxs/store';
 import { RequestForAccessComponent } from './request-for-access.component';
+import { AccessState } from './stores/request-for-access.state';
 
 describe('RequestForAccessComponent', () => {
+  let store: Store;
   let component: RequestForAccessComponent;
   let fixture: ComponentFixture<RequestForAccessComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RequestForAccessComponent ]
+      declarations: [ RequestForAccessComponent ],
+      imports: [MatDividerModule, HttpClientModule, NgxsModule.forRoot([AccessState], { developmentMode: true })]
     })
     .compileComponents();
+
+    store = TestBed.inject(Store);
   });
 
   beforeEach(() => {
@@ -19,7 +26,26 @@ describe('RequestForAccessComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize resource state', () => {
+    store.select( state => state.accessState.accessGranted[0]).subscribe({
+      next: (status) => {
+        expect(status).toBe('Rejected');
+      }
+    });
+  });
+
+  it('should change state to "Pending"', () => {
+    const spy = jest.spyOn(component, 'onClick');
+    component.onClick(0,'Academic Record');
+    expect(spy).toHaveBeenCalled();
+    store.select( state => state.accessState.accessGranted[0]).subscribe({
+      next: (status) => {
+        expect(status).toBe('Pending');
+      }
+    });
   });
 });
--- libs/client/request-for-access/feature/src/lib/services/request-for-access.service.spec.ts
+++ libs/client/request-for-access/feature/src/lib/services/request-for-access.service.spec.ts
@@ -1,15 +1,45 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
 import { RequestForAccessService } from './request-for-access.service';
 
 describe('RequestForAccessService', () => {
   let service: RequestForAccessService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(RequestForAccessService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should be called', () => {
+    const spy = jest.spyOn(service, 'getResourceStatuses');
+
+    service.getResourceStatuses('34232','3222');
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should fetch resource statuses from API', () => {
+    const result = service.getResourceStatuses('4', '2');
+    expect(result).toMatchObject(new Observable<any>());
+
+    result.subscribe({
+      next: (data) => {
+        expect(data).toBeDefined();
+      }
+    });
+  });
+
+  it('should request access from the API', () => {
+    const spy = jest.spyOn(service, 'requestAccess');
+    expect(service.requestAccess('','','')).toBeUndefined();
+    expect(spy).toHaveBeenCalled();
+  });
+
 });
