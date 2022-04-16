2c42361c3499f19cd9c9afc110f4bdc8edf57167
Xander <u20465026@tuks.co.za>
2022-04-16 15:58:33 +0200

    🐛 (api):reverted change

--- libs/api/student-profiles/service/feature/src/lib/student-profiles-service.module.ts
+++ libs/api/student-profiles/service/feature/src/lib/student-profiles-service.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CommandBus } from '@nestjs/cqrs';
 import { ApiStudentProfileService } from './api-student-profiles.service';
 import { CommandsService } from './commands/commands.service';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [ApiStudentProfileService, CommandsService, CommandBus],
+  providers: [ApiStudentProfileService, CommandsService],
   exports: [ApiStudentProfileService],
 })
 export class ApiStudentProfilesServiceModule {}
