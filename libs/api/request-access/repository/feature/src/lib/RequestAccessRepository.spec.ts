e810ff644112d0a873451d731023c7e740727d8c
Phadimane Lerefolo <u19081082@tuks.co.za>
2022-04-16 19:35:25 +0200

    ✅ (added tests); I added tests to test my methods(insert and find request)

--- libs/api/request-access/repository/feature/src/lib/RequestAccessRepository.spec.ts
+++ libs/api/request-access/repository/feature/src/lib/RequestAccessRepository.spec.ts
@@ -3,3 +3,29 @@ import { PrismaService } from '@graduates/api/shared/services/prisma/data-access
 
 
 import { Test, TestingModule } from '@nestjs/testing';
+describe('ApiRequestAccessRepository', () => {
+    let data:RequestAccessRepository;
+    const prisma = new PrismaService();
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        controllers: [RequestAccessRepository],
+        providers: [PrismaService],
+      }).compile();
+      data = module.get<RequestAccessRepository>(RequestAccessRepository);
+      await prisma.requested.create({data: {
+          StudId: 'srdghbfhyn',
+          CompId: 'sgtdnhbxnz',
+          ItemId: 'ACADEMIC_RECORD',
+          Accepted: false
+          }});
+    });
+    it('generic test', async () => {
+      const call1 = await data.insertRequest("1322","1322","CV",false);
+      const call2 = await data.findRequestByStudIdCompId("1322","1322");
+      console.log(call1);
+      console.log(call2[0]);
+      console.log(call2[1]);
+     //expect(call).toBe(null);
+     expect(true).toBeTruthy();
+   });
+  });
