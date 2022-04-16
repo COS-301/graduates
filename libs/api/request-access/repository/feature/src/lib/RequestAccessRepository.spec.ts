f4b4fe9f0ddccde8061f9018037237f5e603b16c
Phadimane Lerefolo <u19081082@tuks.co.za>
2022-04-16 19:32:30 +0200

    ✨ (created testFile);I added a new file for testing my CRUD methods

---
+++ libs/api/request-access/repository/feature/src/lib/RequestAccessRepository.spec.ts
@@ -0,0 +1,5 @@
+import {RequestAccessRepository} from '../lib/RequestAccessRepository.repository';
+import { PrismaService } from '@graduates/api/shared/services/prisma/data-access';
+
+
+import { Test, TestingModule } from '@nestjs/testing';
