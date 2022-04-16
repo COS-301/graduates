fd7fb623c8c838a2e6d416d46a57f3c650e71d55
Kearon Foba <23222438+KearonF@users.noreply.github.com>
2022-04-16 11:15:11 +0200

    ✅ (Shorts-e2e): added more e2e tests

--- apps/client-e2e/src/integration/app.spec.ts
+++ apps/client-e2e/src/integration/app.spec.ts
@@ -15,11 +15,48 @@ describe('client', () => {
 });*/
 
 describe('client-shorts-feature e2e test', () => {
-  beforeEach(() => cy.visit('/shorts'));
 
-  it('should display shorts page', () => {
-    cy.get('h1').contains('Student Shorts');
+  describe('Explore Component Tests', () => {
+    beforeEach(() => cy.visit('/shorts'));
+
+    it('should display shorts page', () => {
+      cy.get('h1').contains('Student Shorts');
+    });
+
+    it('should click short view and close the view', () => {
+      cy.get('.mat-grid-tile:first > .mat-grid-tile-content > #card > .mat-card-content > .grid > :nth-child(1) > #cardHeader > #cardbuttton').click();
+      cy.get('.formbuttonblue').contains('Close').click();
+    });
+
+    it('should make and submit a report', () => {
+      cy.get('.mat-grid-tile:first > .mat-grid-tile-content > #card > .mat-card-content > .grid > :nth-child(1) > #cardHeader > #cardbuttton').click();
+      cy.get('.formbuttonred').contains('Report').click();
+      cy.get('#reason').type('This is a test report');
+      cy.get('.formbuttonred').contains('Submit').click();
+      cy.get('.formbuttonblue').contains('Continue').click();
+    });
+
+    it('tests navigating between tabs', () => {
+      cy.get('#curBtn').should('contain.text', '1');
+      cy.get('.formbuttonblue').contains('Next').click();
+      cy.get('#curBtn').should('contain.text', '2');
+      cy.get('.formbuttonblue').contains('Prev').click();
+      cy.get('#curBtn').should('contain.text', '1');
+    });
   });
 
+  describe.only('upload component tests', () => {
+    beforeEach(() => cy.visit('/shorts/upload'));
+
+    it('should display then cancel upload component', () => {
+      cy.get('.formbuttonblue').contains('Cancel').click();
+    });
+    it('should upload a cat video', () => {
+      cy.get('#uploadbanner').contains('Upload');
+      cy.get('input[type="file"]').selectFile('src/fixtures/test.mp4');
+      cy.get('#taginput').type('#cats #test');
+      cy.get('.formbuttonblue').contains('Submit').click();
+    });
+  });
 
 });
