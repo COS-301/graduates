a504e23f59aa18a0c00fa033923c42af82b85ff8
Xander <u20465026@tuks.co.za>
2022-04-17 00:52:33 +0200

    🐛 (api):added needed import in service library

--- libs/api/student-profiles/service/feature/src/lib/student-profiles-service.module.ts
+++ libs/api/student-profiles/service/feature/src/lib/student-profiles-service.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ApiStudentProfileService } from './api-student-profiles.service';
 import * as QueryHandlers from './queries/handlers';
+import { StudentProfilesRepository } from '@graduates/api/student-profiles/repository/data-access';
 @Module({
   imports: [ApiStudentProfilesRepositoryDataAccessModule, CqrsModule],
   controllers: [],
-  providers: [ApiStudentProfileService, QueryHandlers.GetStudentProfileNameHandler],
+  providers: [ApiStudentProfileService, QueryHandlers.GetStudentProfileNameHandler, StudentProfilesRepository],
   exports: [ApiStudentProfileService],
 })
 export class ApiStudentProfilesServiceModule {}
