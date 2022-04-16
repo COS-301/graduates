b4199123e02b9d8b569a15ec33220de3a46aaf2e
Michael du Plessis <mdp0101@gmail.com>
2022-04-16 15:33:14 +0200

    :sparkles: If an item has not been requested than it is still returned in status as private

--- libs/api/request-access/api/feature/src/lib/api-request-access.resolver.ts
+++ libs/api/request-access/api/feature/src/lib/api-request-access.resolver.ts
@@ -1,12 +1,15 @@
 import { RequestAccessService } from "@graduates/api/request-access/service/feature";
+import { UseGuards } from "@nestjs/common";
 import { Args, ID, Mutation, Resolver } from "@nestjs/graphql";
 import { ApiRequestAccessEntity } from "./api-request-access.entity";
+// import { JwtAuthGuard } from "@graduates/api/auth"
 
 @Resolver(of => ApiRequestAccessEntity)
 export class ApiRequestAccessResolver {
     constructor(private requestAccessService: RequestAccessService) {}
 
     @Mutation(returns => ApiRequestAccessEntity, { nullable: true })
+    // @UseGuards(JwtAuthGuard)
     // must add paramters to request
     async requestAccess(@Args('compId', { type: () => ID }) compId: string, @Args('gradId', { type: () => ID }) gradId: string, @Args('item') item: string): Promise<ApiRequestAccessEntity> {
         enum Items { // if new items become available add here
