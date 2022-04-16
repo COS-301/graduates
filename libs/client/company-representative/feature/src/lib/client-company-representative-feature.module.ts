5cd7d6563b71a8a952096f2939cefa85044a9f5c
Ryan Broemer <u20519517@tuks.co.za>
2022-04-16 21:58:36 +0200

    💄 (UI) : updating company-representative-feature

--- libs/client/company-representative/feature/src/lib/client-company-representative-feature.module.ts
+++ libs/client/company-representative/feature/src/lib/client-company-representative-feature.module.ts
@@ -1,22 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CompanyRepresentativePageComponent } from './company-representative-page/company-representative-page.component';
-import { CompanyRepresentativeAboutComponent } from './company-representative-about/company-representative-about.component';
-import { CompanyRepresentativeTitleComponent } from './company-representative-title/company-representative-title.component';
-import { CompanyRepresentativeExperienceComponent } from './company-representative-experience/company-representative-experience.component';
-import { CompanyRepresentativeConnectionsComponent } from './company-representative-connections/company-representative-connections.component';
-import { CompanyRepresentativeContactComponent } from './company-representative-contact/company-representative-contact.component';
 import { MatCardModule } from '@angular/material/card';
 import { CompanyRepresentativeRoutingModule } from "./company-representative-routing.module";
 import { CompanyRepresentativeMockLoginPageComponent } from './company-representative-mock-login-page/company-representative-mock-login-page.component';
-import { CompanyRepresentativeExploreComponent } from './company-representative-explore/company-representative-explore.component';
 import { CompanyRepresentativeMockStudentExplorePageComponent } from './company-representative-mock-student-explore-page/company-representative-mock-student-explore-page.component';
 import { MatButtonModule } from '@angular/material/button';
 import { CompanyRepresentativeEditPageComponent } from './company-representative-edit-page/company-representative-edit-page.component';
-import { CompanyRepresentativeEditTitleComponent } from './company-representative-edit-page/company-representative-edit-title/company-representative-edit-title.component';
-import { CompanyRepresentativeEditExperienceComponent } from './company-representative-edit-page/company-representative-edit-experience/company-representative-edit-experience.component';
-import { CompanyRepresentativeEditAboutComponent } from './company-representative-edit-page/company-representative-edit-about/company-representative-edit-about.component';
-import { CompanyRepresentativeEditContactComponent } from './company-representative-edit-page/company-representative-edit-contact/company-representative-edit-contact.component';
 
 @NgModule({
   imports: [
@@ -27,19 +17,9 @@ import { CompanyRepresentativeEditContactComponent } from './company-representat
   ],
   declarations: [
     CompanyRepresentativePageComponent,
-    CompanyRepresentativeAboutComponent,
-    CompanyRepresentativeTitleComponent,
-    CompanyRepresentativeExperienceComponent,
-    CompanyRepresentativeConnectionsComponent,
-    CompanyRepresentativeContactComponent,
     CompanyRepresentativeMockLoginPageComponent,
-    CompanyRepresentativeExploreComponent,
     CompanyRepresentativeMockStudentExplorePageComponent,
     CompanyRepresentativeEditPageComponent,
-    CompanyRepresentativeEditTitleComponent,
-    CompanyRepresentativeEditExperienceComponent,
-    CompanyRepresentativeEditAboutComponent,
-    CompanyRepresentativeEditContactComponent
   ],
   exports : [
     CompanyRepresentativeMockStudentExplorePageComponent,
