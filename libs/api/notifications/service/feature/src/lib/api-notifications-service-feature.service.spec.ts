85cd4826ee9ad0daea44b8cb36962635f4db837f
OomBen <u17030553@tuks.co.za>
2022-04-17 00:57:47 +0200

    Added tests to notification service queries

--- libs/api/notifications/service/feature/src/lib/api-notifications-service-feature.service.spec.ts
+++ libs/api/notifications/service/feature/src/lib/api-notifications-service-feature.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test } from '@nestjs/testing';
 import { CommandBus, QueryBus, EventBus } from '@nestjs/cqrs';
 import { ApiNotificationsService } from './api-notifications-service-feature.service';
+import { ModuleRef } from '@nestjs/core';
 
 describe('ApiNotificationsService', () => {
   let service: ApiNotificationsService;
@@ -16,4 +17,31 @@ describe('ApiNotificationsService', () => {
   it('should be defined', () => {
     expect(service).toBeTruthy();
   });
+
+  //Start of Manually added testing
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should be defined', async () => {
+    const creator = jest.spyOn(service,'sendToMail');
+    const result = await service.sendToMail("emailFrom","emailTo","Subject","text");
+    expect(creator).toBeCalled();
+    expect(result).toBeUndefined();
+  });
+
+  it('should be defined', async () => {
+    const creator = jest.spyOn(service,'sendToMail');
+    const result = await service.sendToMail("emailFrom","emailTo","Subject","text");
+    expect(creator).toBeCalled();
+    expect(result).toBeUndefined();
+  });
+
+/*   it('should be defined', async () => {
+    const creator = jest.spyOn(service,'getAllNoifications');
+    const result = await service.getAllNoifications();
+    expect(creator).toBeCalled();
+    expect(result).toBeInstanceOf(Promise);
+  }); */
+
 });
