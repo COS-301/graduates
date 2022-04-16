528789740471063285ee944add859678dc7334be
Zenthon <u20536951@tuks.co.za>
2022-04-16 11:06:24 +0200

    🔥 Minor changes

--- libs/api/company-representative/service/feature/src/lib/queries/handlers/getOneRepresentativeHandler.handler.ts
+++ libs/api/company-representative/service/feature/src/lib/queries/handlers/getOneRepresentativeHandler.handler.ts
@@ -2,6 +2,7 @@ import { IQueryHandler,QueryHandler } from "@nestjs/cqrs";
 import { User } from "@prisma/client";
 import { GetOneRepresentative } from "../impl/getOneRepresentative.query";
 import { CompanyRepresentativeRepository } from "@graduates/api/company-representative/repository/data-access";
+import { CompanyRepresentative } from "@graduates/api/company-representative/api/shared/data-access";
 
 @QueryHandler(GetOneRepresentative)
 export class GetOneRepresentativeHandler implements IQueryHandler<GetOneRepresentative>{
