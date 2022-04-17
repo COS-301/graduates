23c2e8672d051691b435724505aa97caa77d0358
Isakvdw <ipmegladon@gmail.com>
2022-04-17 14:37:58 +0200

    🐛 Accidental file add

--- libs/api/companyexplore/api/shared/interfaces/data-access/src/lib/api-companyexploreUser.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ApiCompanyExploreUserEntity } from './api-companyexploreUser.entity';
-
-describe('ApiCompanyExploreUserEntity', () => {
-  it('should be defined', () => {
-    expect(new ApiCompanyExploreUserEntity()).toBeDefined();
-  });
-});
