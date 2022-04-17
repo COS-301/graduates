4fd3e73ee8423b084f9ba0cecab2264dbe74ce2b
TimovdMerwe-18 <u19048280@tuks.co.za>
2022-04-17 10:14:19 +0200

    Shell File Fix

--- libs/api/shell/feature/src/lib/api-shell-feature.module.ts
+++ libs/api/shell/feature/src/lib/api-shell-feature.module.ts
@@ -1,14 +1,14 @@
 import { FeatureModule as ApiExampleFeatureModule } from '@graduates/api/example/api/feature';
 import { ApiStorageApiFeatureModule as ApiStorageFeatureModule } from '@graduates/api/storage/api/feature';
 import { ApiStudentProfilesModule as ApiStudentProfilesModule } from '@graduates/api/student-profiles/api/feature';
-//import { ApiHostingApiFeatureModule as ApiHostingFeatureModule } from '@graduates/api/hosting/api/feature';
+import { ApiHostingApiFeatureModule as ApiHostingFeatureModule } from '@graduates/api/hosting/api/feature';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { ApiAccessStatusApiFeatureModule as ApiAccessStatusFeatureModule} from '@graduates/api/access-status/api/feature';
 import { ApiShortsApiFeatureModule as ApiShortsFeatureModule } from '@graduates/api/shorts/api/feature';
 import { ApicompanyprofilepageApiFeatureModule as ApiCompanyProfilePageFeatureModule } from '@graduates/api/companyprofilepage/api/feature';
-//import { ApiCompanyRepresentativeApiFeatureModule } from '@graduates/api/company-representative/api/feature';
+import { ApiCompanyRepresentativeApiFeatureModule } from '@graduates/api/company-representative/api/feature';
 import { ApiRequestAccessApiFeatureModule as ApiRequestAccessFeatureModule } from '@graduates/api/request-access/api/feature';
 import { ApiAuthenticationApiFeatureModule } from '@graduates/api/authentication/api/feature'
 import { ApiNotificationsApiFeatureModule } from '@graduates/api/notifications/api/feature'
@@ -26,17 +26,17 @@ import { ApiBlogApiFeatureModule } from '@graduates/api/blog/api/feature';
       driver: ApolloDriver,
     }),
     ApiStorageFeatureModule,
-    //ApiHostingFeatureModule,
-    // ApiShortsFeatureModule,
-    // ApiNotificationsApiFeatureModule,
+    ApiHostingFeatureModule,
+    ApiShortsFeatureModule,
+    ApiNotificationsApiFeatureModule,
     ApiAccessStatusFeatureModule,
-    //ApiCompanyProfilePageFeatureModule,
-    //ApiStudentProfilesModule,
-    //ApiCompanyRepresentativeApiFeatureModule,
+    ApiCompanyProfilePageFeatureModule,
+    ApiStudentProfilesModule,
+    ApiCompanyRepresentativeApiFeatureModule,
     ApiRequestAccessFeatureModule,
     ApiAuthenticationApiFeatureModule,
     ApiAuthorizationFeatureModule,
-    // ApiStudentExploreApiFeatureModule,
+    ApiStudentExploreApiFeatureModule,
     ApiAdminconsoleApiFeatureModule,
     ApiUpintegrationFeatureModule,
     ApiBlogApiFeatureModule
