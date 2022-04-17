96f417fe59c3392edce14d31d355ff4b34ae76fb
Dylan Pietersen <61905387+dylan2894@users.noreply.github.com>
2022-04-17 13:23:14 +0200

    formatting

--- libs/client/request-for-access/feature/src/lib/request-for-access.component.spec.ts
+++ libs/client/request-for-access/feature/src/lib/request-for-access.component.spec.ts
@@ -1,21 +1,24 @@
-import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDividerModule } from '@angular/material/divider';
-import { NgxsModule, Store } from '@ngxs/store';
-import { RequestForAccessComponent } from './request-for-access.component';
-import { AccessState } from './stores/request-for-access.state';
+import { HttpClientModule } from "@angular/common/http";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatDividerModule } from "@angular/material/divider";
+import { NgxsModule, Store } from "@ngxs/store";
+import { RequestForAccessComponent } from "./request-for-access.component";
+import { AccessState } from "./stores/request-for-access.state";
 
-describe('RequestForAccessComponent', () => {
+describe("RequestForAccessComponent", () => {
   let store: Store;
   let component: RequestForAccessComponent;
   let fixture: ComponentFixture<RequestForAccessComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RequestForAccessComponent ],
-      imports: [MatDividerModule, HttpClientModule, NgxsModule.forRoot([AccessState], { developmentMode: true })]
-    })
-    .compileComponents();
+      declarations: [RequestForAccessComponent],
+      imports: [
+        MatDividerModule,
+        HttpClientModule,
+        NgxsModule.forRoot([AccessState], { developmentMode: true }),
+      ],
+    }).compileComponents();
 
     store = TestBed.inject(Store);
   });
@@ -26,26 +29,30 @@ describe('RequestForAccessComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(component).toBeTruthy();
   });
 
-  it('should initialize resource state', () => {
-    store.select( state => state.accessState.accessGranted[0]).subscribe({
-      next: (status) => {
-        expect(status).toBe('Rejected');
-      }
-    });
+  it("should initialize resource state", () => {
+    store
+      .select((state) => state.accessState.accessGranted[0])
+      .subscribe({
+        next: (status) => {
+          expect(status).toBe("Rejected");
+        },
+      });
   });
 
   it('should change state to "Pending"', () => {
-    const spy = jest.spyOn(component, 'onClick');
-    component.onClick(0,'Academic Record');
+    const spy = jest.spyOn(component, "onClick");
+    component.onClick(0, "Academic Record");
     expect(spy).toHaveBeenCalled();
-    store.select( state => state.accessState.accessGranted[0]).subscribe({
-      next: (status) => {
-        expect(status).toBe('Pending');
-      }
-    });
+    store
+      .select((state) => state.accessState.accessGranted[0])
+      .subscribe({
+        next: (status) => {
+          expect(status).toBe("Pending");
+        },
+      });
   });
 });
