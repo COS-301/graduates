68a77b1d9b590454c3f9b8473cae065ae23b870c
Ryan Broemer <u20519517@tuks.co.za>
2022-04-17 15:43:26 +0200

    💄(UI):Final UI

--- libs/client/company-representative/feature/src/lib/company-representative-service/company-representative-service.service.spec.ts
+++ libs/client/company-representative/feature/src/lib/company-representative-service/company-representative-service.service.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { CompanyRepresentativeServiceService } from './company-representative-service.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('CompanyRepresentativeServiceService', () => {
   let service: CompanyRepresentativeServiceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(CompanyRepresentativeServiceService);
   });
 
