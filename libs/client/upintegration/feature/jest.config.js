d6b7570394f0e04c35b2efc0dd358739599f4d6c
Wesley Pachai <u20578688@tuks.co.za>
2022-04-17 14:55:23 +0200

    🐛(UI): Correcting Directories

--- libs/client/upintegration/feature/jest.config.js
+++ libs/client/upintegration/feature/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'client-upintegration',
-  preset: '../../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../coverage/libs/client/upintegration',
+  coverageDirectory: '../../../../coverage/libs/client/upintegration/feature',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
