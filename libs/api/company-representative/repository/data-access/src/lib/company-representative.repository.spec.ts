3c70547f0ad8b7d5dcf124df9b30d0100b3d8bba
Lehlohonolo-Sehako <u20557622@tuks.co.za>
2022-04-17 07:37:22 +0200

    :white_check_mark: (Tests): added tests

--- libs/api/company-representative/repository/data-access/src/lib/company-representative.repository.spec.ts
+++ libs/api/company-representative/repository/data-access/src/lib/company-representative.repository.spec.ts
@@ -1,6 +1,7 @@
-
 import { Test, TestingModule } from '@nestjs/testing';
 import { CompanyRepresentativeRepository } from './company-representative.repository';
+import { PrismaService } from "@graduates/api/shared/services/prisma/data-access";
+import { UserSocialMedia } from '@prisma/client';
 
 
 describe('CompanyRepresentativeRepository', () => {
@@ -9,18 +10,69 @@ describe('CompanyRepresentativeRepository', () => {
 
     beforeAll(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [CompanyRepresentativeRepository]
+            providers: [CompanyRepresentativeRepository, PrismaService]
         }).compile();
 
         repository = module.get<CompanyRepresentativeRepository>(CompanyRepresentativeRepository);
     });
 
-    
 
     it('Should be defined', async () => {
       expect(repository).toBeDefined();
     });
 
+    describe('@returnRepObject', () => {
+        
+        const id = '1';
+        const repName = 'Tester';
+        const email = 'tester@gmail.com';
+        const jobTitle = 'Tester';
+        const aboutMe = 'Software Tester';
+        const website = 'tester.up.ac.za';
+        const location = 'Hatfield, Pretoria';
+        const phoneNumber = '0812345678';
+        const repExperience = 'Beginner';
+
+        const SocialMedia: UserSocialMedia[] = [
+             {
+                userId: id,
+                type: 'TWITTER',
+                link: 'ishe@twitter.com',
+             },
+             {
+                userId: id,
+                type: "INSTAGRAM",
+                link: "ishe@instagram.com"
+              }, {
+                userId: id,
+                type: "LINKEDIN",
+                link: "linkedin.com/in/isheanesu-dzingirai-2952b9180"
+              }, {
+                userId: id,
+                type: "FACEBOOK",
+                link: "ishe@facebook.com"
+              },{
+                userId: id,
+                type: "SNAPCHAT",
+                link: "ishe@snapchat.com"
+              },{
+                userId: id,
+                type: "GITHUB",
+                link: "zenthon@github.com"
+              }
+        
+          ]
+
+        it('should return a representative object',async () => {
+            try {
+                const user = repository.returnRepObject(id, repName, email, jobTitle, aboutMe, website, SocialMedia, location, phoneNumber, repExperience);
+                expect(user).toEqual(user);
+
+            } catch (error) {
+                fail(error);
+            }
+        })
+    }); 
          
     describe('@login', () => {
 
@@ -40,14 +92,43 @@ describe('CompanyRepresentativeRepository', () => {
 
     describe('@getAllRepresentativeUsers', () => {
 
-        it('should return all representatives', () =>{
+        it('should return all representatives', async () =>{
             try {
                 const allReps = repository.getAllRepresentativeUsers();
                 expect(allReps).toHaveReturned;
             } catch (error) {
                 fail(error);
             }    
-        })
+        });
+    });
+
+    describe('@createDefaultRep', () =>{
+
+        it('should create a default company representative', async () => {
+            try{
+                const defaultRep = repository.createDefaultRep();
+                expect(defaultRep).toHaveBeenCalled;
+            } catch (error) {
+                fail(error);
+            }
+        });
+
+    });
+
+    describe('@deleteRepresentative', () =>{
+
+        const user = 'Tester';
+
+        it('should delete a representative', async () =>{
+            try{
+                
+                const deletedUser = repository.deleteRepresentative(user);
+                expect(deletedUser).toEqual(deletedUser);
+
+            } catch (error) {
+                fail(error);
+            }
+        });
     });
 
 });
