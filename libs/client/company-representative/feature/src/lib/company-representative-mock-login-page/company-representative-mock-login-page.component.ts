5656ecf7abe413115e7318818bc5355cf065f0ac
Sean Nkosi <seann@agilebridge.co.za>
2022-04-17 17:10:12 +0200

    Added onSubmit function

--- libs/client/company-representative/feature/src/lib/company-representative-mock-login-page/company-representative-mock-login-page.component.ts
+++ libs/client/company-representative/feature/src/lib/company-representative-mock-login-page/company-representative-mock-login-page.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import {Router } from '@angular/router';
 
 @Component({
   selector: 'graduates-company-representative-mock-login-page',
   templateUrl: './company-representative-mock-login-page.component.html',
   styleUrls: ['./company-representative-mock-login-page.component.scss']
 })
-export class CompanyRepresentativeMockLoginPageComponent {
-  constructor(private _router: Router) { }
+export class CompanyRepresentativeMockLoginPageComponent implements OnInit{
+  formdata!: FormGroup;
+  flag = false;
+  constructor(private _router: Router) {
+  }
+  ngOnInit() {
+    this.formdata = new FormGroup({
+       email: new FormControl(""),
+       password: new FormControl("")
+    });
+ }
+
 
   navigateToFirst() {
     this._router.navigate(['CompanyRepresentativeHome'])
+  }
+
+  alertBox(){
+    alert("incorrect Login Details!");
+  }
+
+  submit(data: { email: string; password: string; }){
+    alert("email: " + data.email + "Passie: " + data.password);
+    if (this.flag)
+      this.navigateToFirst()
+      else
+      this.alertBox();
   }
 }
