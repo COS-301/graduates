c2d8ed1abee965cd077aaec69efc0a5e33591fd7
Isakvdw <ipmegladon@gmail.com>
2022-04-17 16:50:07 +0200

    ✅ (fix) fixed companyexplore repo failing spec

--- libs/api/companyexplore/repository/data-access/src/lib/api-companyexplore-respository-data-access.spec.ts
+++ libs/api/companyexplore/repository/data-access/src/lib/api-companyexplore-respository-data-access.spec.ts
@@ -1,7 +1,20 @@
 import { CompanyExploreRepository } from './api-companyexplore-repository-data-access';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '@graduates/api/shared/services/prisma/data-access';
 
 describe('CompanyExploreRepository', () => {
+  let repo: CompanyExploreRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [CompanyExploreRepository],
+      providers: [PrismaService],
+    }).compile();
+
+    repo = module.get<CompanyExploreRepository>(CompanyExploreRepository);
+  });
+
   it('should be defined', () => {
-    expect(new CompanyExploreRepository()).toBeDefined();
+    expect(repo).toBeDefined();
   });
 });
