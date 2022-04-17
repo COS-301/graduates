7c0cd8463c505d246f31db6db070df4bfa468e48
Agape <theaman249@gmail.com>
2022-04-17 04:04:10 +0200

    🚧 (services) Created an update servcies layer

--- libs/api/company-representative/service/feature/src/lib/api-company-representative.service.ts
+++ libs/api/company-representative/service/feature/src/lib/api-company-representative.service.ts
@@ -6,7 +6,7 @@ import { DeleteRepresentativeCommand } from './commands/impl/deleteRepresentativ
 import { GetAllRepresentatives } from './queries/impl/getAllRepresentatives.query';
 import { GetCompanyRepresentativeQuery } from './queries/impl/getRepresentative.query';
 import { GetCompanyRepresentativeLoginQuery } from './queries/impl/getRepresentativeLoginID.query';
-
+import { UpdateRepresentative } from './commands/impl/updateRepresentative.command';
 @Injectable()
 export class ApiCompanyRepresentativeService {
 
@@ -20,16 +20,43 @@ export class ApiCompanyRepresentativeService {
     return this.queryBus.execute(new GetCompanyRepresentativeLoginQuery(email, password));
   }  
 
+  /***
+   * 
+   * This function deletes a user by accepting a representative Id
+   */
+
   async deleteRepresentative(repId: string) {
     return this.commandBus.execute(new DeleteRepresentativeCommand(repId));
   }
 
+  /***
+   * This function is used to get All representatives from the datbase
+   */
   async getAllRepresentatives(){
     return this.queryBus.execute(new GetAllRepresentatives());
   }
 
+  /***
+   * This function is used to create a new Representative
+   * *WORK IN PROGRESS!*
+   */
+
   async createRepresentative(){
     return this.commandBus.execute(new CreateRepresentative)
+  }
+
+  /***
+   * Update the attributes of the dataBase accoring to these types of updates:
+   * bio
+   * name
+   * number
+   * expirience
+   * loaction
+   */
+
+  async UpdateRepresentative(id:string,newData:string,type:string)
+  {
+    return this.commandBus.execute(new UpdateRepresentative(id,newData,type))
   }
 
   
--- libs/api/company-representative/service/feature/src/lib/commands/handlers/createRepresentativeHandler.handler.ts
+++ libs/api/company-representative/service/feature/src/lib/commands/handlers/createRepresentativeHandler.handler.ts
@@ -3,14 +3,12 @@ import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { CreateRepresentative } from "../impl/createRepresentative.command";
 
 
-
-
 @CommandHandler(CreateRepresentative)
 export class CreateRepresentativeHandler implements ICommandHandler<CreateRepresentative>
 {
     constructor(private repository:CompanyRepresentativeRepository){}
 
-    execute(command: CreateRepresentative): Promise<any> {
+    execute(command: CreateRepresentative){
         return null; //insert command from repository layer here
     }
 }
