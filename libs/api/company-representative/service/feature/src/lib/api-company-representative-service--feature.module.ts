117461cb39b5361f9a27d486c2b72de80b3c95b0
Isheanesu Joseph Dzingirai <u20536951@tuks.co.za>
2022-04-18 05:32:15 +0200

    🐛 (repository): Removed APIStorage as it was giving issues

--- libs/api/company-representative/service/feature/src/lib/api-company-representative-service--feature.module.ts
+++ libs/api/company-representative/service/feature/src/lib/api-company-representative-service--feature.module.ts
@@ -1,6 +1,5 @@
 import { CompanyRepresentativeRepository } from '@graduates/api/company-representative/repository/data-access';
 import { PrismaService } from '@graduates/api/shared/services/prisma/data-access';
-import { ApiStorageServiceFeatureModule } from '@graduates/api/storage/service/feature';
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ApiCompanyRepresentativeService } from './api-company-representative.service';
@@ -16,7 +15,6 @@ import { QueryHandlers } from './queries/handlers';
     ApiCompanyRepresentativeService,
     PrismaService,
     CompanyRepresentativeRepository,
-    ApiStorageServiceFeatureModule
   ],
   exports: [ApiCompanyRepresentativeService],
 })
