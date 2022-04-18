c82063d3c4124bb83d429ed3aa0421699ce64571
Grobbies26 <74908721+Grobbies26@users.noreply.github.com>
2022-04-18 03:12:24 +0200

    🎨 (up-integration):major changes to api and and attached layers

--- libs/api/upintegration/api/shared/data-access/src/lib/api-upintegration-academicrecord.entity.ts
+++ libs/api/upintegration/api/shared/data-access/src/lib/api-upintegration-academicrecord.entity.ts
@@ -1,38 +1,16 @@
-import { Field, ID, ObjectType } from '@nestjs/graphql';
-import { FileCategory } from '@prisma/client';
+import { Field, ObjectType } from '@nestjs/graphql';
 
 /**
  * Definition of Academic Record entity
  */
 @ObjectType()
 export class AcademicRecord {
-    /**
-     * The users ID
-     */
-    @Field(() => ID)
-    userId!: string;
+    @Field({ nullable: true })
+    fileAsString!: string;
+}
 
-    /**
-     * The file category
-     */
-    @Field(()=> FileCategory)
-    fileCategory!: FileCategory;
-
-    /**
-     * The file extension
-     */
-    @Field()
-    fileExtension!: string;
-
-    /**
-     * The file Path
-     */
-    @Field({ nullable: true})
-    filePath!: string;
-
-    /**
-     * The file as a string
-     */
-    @Field({ nullable: true})
+@ObjectType()
+export class Degree{
+    @Field({ nullable: true })
     fileAsString!: string;
 }
--- libs/api/upintegration/repository/data-access/src/lib/api-upintegration-repository-data-access.module.spec.ts
+++ libs/api/upintegration/repository/data-access/src/lib/api-upintegration-repository-data-access.module.spec.ts
@@ -1,10 +1,10 @@
 import { async, TestBed } from '@angular/core/testing';
-import { ApiUpintegrationRepositoryDataAccessModule } from './api-upintegration-repository-data-access.module';
+import { UpIntegrationRepository } from './api-upintegration-repository-data-access';
 
 describe('ApiUpintegrationRepositoryDataAccessModule', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ApiUpintegrationRepositoryDataAccessModule],
+      imports: [UpIntegrationRepository],
     }).compileComponents();
   }));
 
@@ -13,6 +13,6 @@ describe('ApiUpintegrationRepositoryDataAccessModule', () => {
   // NB: This particular test does not do anything useful.
   //     It does NOT check for correct instantiation of the module.
   it('should have a module definition', () => {
-    expect(ApiUpintegrationRepositoryDataAccessModule).toBeDefined();
+    expect(UpIntegrationRepository).toBeDefined();
   });
 });
