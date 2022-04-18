3f66c0039a68a01869bfe8488fff3c775993c57e
neoseefane <u19114738@tuks.co.za>
2022-04-18 07:01:11 +0200

    🗑️ (resolver) commented out code causing issues

--- libs/api/adminconsole/api/feature/src/lib/api-adminconsole.resolver.ts
+++ libs/api/adminconsole/api/feature/src/lib/api-adminconsole.resolver.ts
@@ -1,10 +1,10 @@
-import { ApiAdminConsoleServiceFeature } from "@graduates/api/adminconsole/service/feature";
+// import { ApiAdminConsoleServiceFeature } from "@graduates/api/adminconsole/service/feature";
 import { Resolver, Query } from "@nestjs/graphql";
 //import { ApiAdminConsole } from "@graduates/api/adminconsole/api/repository/data-access";
 import {User} from "@graduates/api/adminconsole/repository/data-access";
 @Resolver(() => User)
 export class ApiAdminConsoleResolver{
-    constructor(private adminService: ApiAdminConsoleServiceFeature){}
+    // constructor(private adminService: ApiAdminConsoleServiceFeature){}
 
     // @Query(() => [User])
     // adminconsole(): Promise<User[]>{
