189017eb7f7307c73acb66bfc51fb1d04f8493f7
neoseefane <u19114738@tuks.co.za>
2022-04-18 23:52:48 +0200

    🔥 (spec) errors

--- libs/api/adminconsole/api/feature/src/lib/api-adminconsole.resolver.spec.ts
+++ libs/api/adminconsole/api/feature/src/lib/api-adminconsole.resolver.spec.ts
@@ -1,16 +1,22 @@
-import { ApiAdminConsoleServiceFeature} from '@graduates/api/adminconsole/service/feature';
+/*import { ApiAdminConsoleServiceFeature} from '@graduates/api/adminconsole/service/feature';*/
+import { PrismaService } from '@graduates/api/shared/services/prisma/data-access';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApiAdminConsoleResolver } from './api-adminconsole.resolver';
 
 describe('ApiAdminConsoleResolver', () => {
   let resolver: ApiAdminConsoleResolver;
-
+  let queryBus: QueryBus;
+ let commandBus: CommandBus;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ApiAdminConsoleResolver, ApiAdminConsoleServiceFeature],
+      controllers: [ApiAdminConsoleResolver],
+      providers: [PrismaService, QueryBus, CommandBus],
     }).compile();
 
     resolver = module.get<ApiAdminConsoleResolver>(ApiAdminConsoleResolver);
+    commandBus = module.get<CommandBus>(CommandBus);
+    queryBus = module.get<QueryBus>(QueryBus);
   });
   it('should be created', () => {
     expect(resolver).toBeTruthy();
