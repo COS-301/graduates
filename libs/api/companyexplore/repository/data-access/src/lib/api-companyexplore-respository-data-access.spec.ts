dc4677c953c3ad49fe370e6ffbe1bfacc910b4cb
JG (Hanroe) Geyser <u20450932@tuks.co.za>
2022-04-18 19:49:59 +0200

    ✅ (Company-Explore) : Removed unnneeded tests

--- libs/api/companyexplore/repository/data-access/src/lib/api-companyexplore-respository-data-access.spec.ts
+++ libs/api/companyexplore/repository/data-access/src/lib/api-companyexplore-respository-data-access.spec.ts
@@ -16,6 +16,30 @@ describe('CompanyExploreRepository', () => {
   it('should be defined', () => {
     expect(repo).toBeDefined();
   });
+
+  it(': Testing getDefaultCompany',async () => {
+    const result = await repo.getDefaultCompany();
+    //console.log(result);
+    expect(result);
+  });
+
+  it(': Testing getCompanyByID', async () => {
+    const result = await repo.getCompanyById("123456789");
+    //console.log(result);
+    expect(result);
+  });
+
+  it(': Testing getSearchResults', async () => {
+    const result = await repo.getSearchResults("null3");
+    //console.log(result);
+    expect(result);
+  });
+
+  it(': Testing getTaggedCompany', async () => {
+    const result = await repo.getTaggedCompany("nice");
+    //console.log(result);
+    expect(result);
+  });
 });
 
 
