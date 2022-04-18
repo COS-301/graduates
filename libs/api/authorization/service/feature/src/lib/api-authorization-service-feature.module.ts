4470607c4e6aeea144ebe7a28506229b34e5ee79
Raymond Boonzaier <u19014725@tuks.co.za>
2022-04-18 08:41:21 +0200

    ✅  (service) try fix linting

--- libs/api/authorization/service/feature/src/lib/api-authorization-service-feature.module.ts
+++ libs/api/authorization/service/feature/src/lib/api-authorization-service-feature.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
+import { GetDeletePermissionHandler } from '../lib/queries/handlers/get-delete-permissions.handler';
+import { GetEditPermissionHandler } from '../lib/queries/handlers/get-edit-permissions.handler';
+import { GetViewPermissionHandler } from '../lib/queries/handlers/get-view-permissions.handler';
 import { ApiAuthorizationService } from './api-authorization.service';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { Adminauthorization } from '../../../../repository/data-access/src/lib/api-authorization-repository-admin.repository';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { PrismaService } from '../../../../../shared/services/prisma/data-access/src/lib/ApiPrismaService.service';
+import { QueryBus } from '@nestjs/cqrs';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { ApiAuthorization } from '../../../../api/shared/src/lib/api-authorization.entity';
 
 @Module({
   controllers: [],
-  providers: [ApiAuthorizationService],
+  imports: [],
+  providers: [
+    ApiAuthorization,
+    ApiAuthorizationService,
+    QueryBus,
+    Adminauthorization,
+    PrismaService,
+    GetDeletePermissionHandler,
+    GetEditPermissionHandler,
+    GetViewPermissionHandler,
+  ],
   exports: [ApiAuthorizationService],
 })
-
-
 export class ApiAuthorizationServiceFeatureModule {}
