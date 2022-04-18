65e01d29423035ab5748d29adf5117873250db82
Kearon Foba <23222438+KearonF@users.noreply.github.com>
2022-04-18 20:53:31 +0200

    🎨 ✅  (Shorts-E2E): Made visual adjustments to code

--- apps/client-e2e/src/integration/client.shorts.feature.spec.ts
+++ apps/client-e2e/src/integration/client.shorts.feature.spec.ts
@@ -6,24 +6,24 @@ describe('client-shorts-feature e2e test', () => {
         cy.wait('@getall');
     });
       it('should click a short view and close the view', () => {
-        cy.get('.formbutton:first').contains('View').click();
-        cy.get('.text-right > .formbuttonblue').contains('Close').click();
+        cy.get('.formbutton').contains('View').click();
+        cy.get('.formbuttonblue').contains('Close').click();
       });
   
-      it('should make and submit a report', () => {
-        cy.get('.formbutton').contains('View').click();
+      
+      /** Can Only work if a short is not reported. I.E Cannot determine if short already reported.
+       Without seed data, this is not deterministic. Manually tested instead.
+       */
+      it.skip('should make and submit a report', () => {
+        cy.get('.formbutton:first').contains('View').click();
         cy.get('.formbuttonred').contains('Report').click();
         cy.get('#reason').type('This is a test report for testing');
         cy.get('.formbuttonred').contains('Submit').click();
-        // cy.get('#uploadbanner').should('contain', 'Report Successful');
-        // cy.get('.formbuttonblue').contains('Continue').click();
-        cy.get('.formbuttonblue').contains('Cancel').click();
-        cy.get('.text-right > .formbuttonblue').contains('Close').click();
-        
+        cy.intercept("/graphql", (res)=>{
+          expect(res.body).to.contain('data');
+        });
       });
-  
-      // TODO Recreate when nav is updated
-      it('tests navigating between tabs', () => {
+        it('tests navigating between tabs', () => {
         cy.get('#curBtn').should('contain.text', '1');
         cy.get('.formbuttonblue').contains('Next').then((nextBtn)=>{
           if(nextBtn.is('enabled')){
@@ -34,8 +34,9 @@ describe('client-shorts-feature e2e test', () => {
         }});
       });
 
-      it.only('should test the search capability', () => {
-        cy.get('#search').type('Testman {enter}');
+      it('should test the search capability', () => {
+        cy.get('#search').type('Testman{enter}');
+        cy.get('#cardHeader').should('contain.text', 'Testman');
       });
     });
   
@@ -49,7 +50,9 @@ describe('client-shorts-feature e2e test', () => {
       it('should display then cancel upload component', () => {
         cy.get('.formbuttonblue').contains('Cancel').click();
       });
-      it('should upload a video and thumbnail', () => {
+
+      //! Test File Upload after feature is pushed
+      it.skip('should upload a video and thumbnail', () => {
         cy.get('#uploadbanner').contains('Upload');
         cy.get('input[type="file"]:first').selectFile('src/fixtures/client-shorts-test-video.mp4');
         cy.get('input[type="file"]:last').selectFile('src/fixtures/client-shorts-test-thumbnail.jpg');
@@ -59,5 +62,4 @@ describe('client-shorts-feature e2e test', () => {
         // cy.get('.formbuttonblue').contains('Cancel').click();
       });
     });
-  
   });
