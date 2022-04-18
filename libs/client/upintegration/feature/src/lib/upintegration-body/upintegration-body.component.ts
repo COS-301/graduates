04006f058f3927f7b4657bcd9dfb619b9f5dd89f
Wesley Pachai <u20578688@tuks.co.za>
2022-04-18 23:13:26 +0200

    ✨(UI): Receives Student Number and Choices

--- libs/client/upintegration/feature/src/lib/upintegration-body/upintegration-body.component.ts
+++ libs/client/upintegration/feature/src/lib/upintegration-body/upintegration-body.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './upintegration-body.component.html',
   styleUrls: ['./upintegration-body.component.scss'],
 })
-export class UpintegrationBodyComponent {}
+export class UpintegrationBodyComponent {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  log(x: any) {
+    let studentNumber = '';
+    studentNumber = x.value;
+  }
+  submit(inputForm: { value: any }) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const permissions = inputForm.value;
+  }
+}
