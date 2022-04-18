e1fc26be47d323eeebee3ec0f99e8ce120e916c3
mattreed01 <matthewreed0107@gmail.com>
2022-04-18 10:00:38 +0200

    🔥 (student-explore): Removed comment I made

--- libs/api/authorization/api/feature/src/lib/api-authorization-api-feature.module.ts
+++ libs/api/authorization/api/feature/src/lib/api-authorization-api-feature.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ApiAuthorizationServiceFeatureModule } from '@graduates/api/authorization/service/feature';
 import { ApiAuthorizationResolver } from './api-authorization.resolver';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { ApiAuthorizationService } from /*'../../../shared/src/lib/api-authorization.entity';*/ '@graduates/api/authorization/service/feature'
+import { ApiAuthorizationService } from '@graduates/api/authorization/service/feature'
 
 @Module({
   controllers: [],
