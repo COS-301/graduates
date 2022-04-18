ca2044918ea9c27f497d7efff1e2a2c1d646190a
OomBen <u17030553@tuks.co.za>
2022-04-18 19:21:02 +0200

    Finalized client side unit testing

--- apps/client-e2e/src/integration/app.spec.ts
+++ apps/client-e2e/src/integration/app.spec.ts
@@ -1,6 +1,7 @@
 // e2e testing to go here please seee below for example
 
 //To run the cypress test suite use the command: "yarn nx run-many --target=e2e --all"
+import { method } from 'cypress/types/bluebird';
 import { getGreeting } from '../support/app.po';
 
 //Example Test
@@ -19,9 +20,31 @@ describe('client', () => {
 describe('client notifications testing', () => {
   beforeEach(() => cy.visit('/notifications'));
 
+  //Test if it redirects to the right feature. (Checks if "notifications" is part of the URL)
+  it('should contain notification board', () => {
+    cy.url().should('include','notifications');
+  });
+
   //Test if the main notifications component has rendered properly
   it('should contain notification board', () => {
     cy.contains('Notification board');
+    cy.get('button').click();
+  });
+
+  //Test if a notificationquery can be called from client and be returned from DB via API
+  it('should return a response', () => {
+      const queryString = `query {
+        notificationsAll {
+            id,
+            data{notificationType},
+            userIdTo,
+            userIdFrom
+        }
+    }`
+
+    cy.intercept({method: "POST", path:"notificationsAll"});
+    cy.reload();
+    cy.wait('@GetNotificationsAll');
   });
 
 });
