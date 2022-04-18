a545a42473b9933d2761a377b4662a24e76bfc3a
Kearon Foba <23222438+KearonF@users.noreply.github.com>
2022-04-18 14:53:18 +0200

    :white_check_mark: :construction: (Shorts-e2e): Test creation in progress

--- apps/client-e2e/src/integration/client.shorts.feature.spec.ts
+++ apps/client-e2e/src/integration/client.shorts.feature.spec.ts
@@ -1,13 +1,39 @@
+
 describe('client-shorts-feature e2e test', () => {
 
+  const mockData = {getAllShorts: {data: [
+    {user:{name:'John',__typename:'User'},shortTag:[],userId:'cl24nyhpy00241cprq7rj3br9',id:'cl24o2z9u01411cpr5wus47gf',thumbnail:,__typename:'Short'},
+    {user:{name:'John',__typename:'User'},shortTag:[],userId:'cl24nyhpy00241cprq7rj3br9',id:'cl24o2z9u01411cpr5wus47gf',thumbnail:null,__typename:'Short'},
+    {user:{name:'John',__typename:'User'},shortTag:[],userId:'cl24nyhpy00241cprq7rj3br9',id:'cl24o2z9u01411cpr5wus47gf',thumbnail:null,__typename:'Short'},
+    {user:{name:'John',__typename:'User'},shortTag:[],userId:'cl24nyhpy00241cprq7rj3br9',id:'cl24o2z9u01411cpr5wus47gf',thumbnail:null,__typename:'Short'},
+    {user:{name:'John',__typename:'User'},shortTag:[],userId:'cl24nyhpy00241cprq7rj3br9',id:'cl24o2z9u01411cpr5wus47gf',thumbnail:null,__typename:'Short'},
+    {user:{name:'John',__typename:'User'},shortTag:[],userId:'cl24nyhpy00241cprq7rj3br9',id:'cl24o2z9u01411cpr5wus47gf',thumbnail:null,__typename:'Short'},
+    {user:{name:'John',__typename:'User'},shortTag:[],userId:'cl24nyhpy00241cprq7rj3br9',id:'cl24o2z9u01411cpr5wus47gf',thumbnail:null,__typename:'Short'},
+    {user:{name:'John',__typename:'User'},shortTag:[],userId:'cl24nyhpy00241cprq7rj3br9',id:'cl24o2z9u01411cpr5wus47gf',thumbnail:null,__typename:'Short'},
+    {user:{name:'John',__typename:'User'},shortTag:[],userId:'cl24nyhpy00241cprq7rj3br9',id:'cl24o2z9u01411cpr5wus47gf',thumbnail:null,__typename:'Short'},
+    {user:{name:'John',__typename:'User'},shortTag:[],userId:'cl24nyhpy00241cprq7rj3br9',id:'cl24o2z9u01411cpr5wus47gf',thumbnail:null,__typename:'Short'},
+    {user:{name:'John',__typename:'User'},shortTag:[],userId:'cl24nyhpy00241cprq7rj3br9',id:'cl24o2z9u01411cpr5wus47gf',thumbnail:null,__typename:'Short'}
+  ]}};
+
+
+
+
     describe('Explore Component Tests', () => {
-      beforeEach(() => cy.visit('/shorts'));
-      it('should click short view and close the view', () => {
-        cy.get('.formbutton:first').contains('View').click();
+      beforeEach(() => {
+        cy.intercept("/graphql", (req)=>{
+          req.reply((res)=>{
+            res.body = mockData;
+          })
+        });
+        // cy.intercept("/graphql");
+        cy.visit('/shorts');
+    });
+      it('should click a short view and close the view', () => {
+        cy.get('.formbutton').contains('View').first().click();
         cy.get('.text-right > .formbuttonblue').contains('Close').click();
       });
   
-      it('should make and submit a report', () => {
+      it.skip('should make and submit a report', () => {
         cy.get('.formbutton').contains('View').click();
         cy.get('.formbuttonred').contains('Report').click();
         cy.get('#reason').type('This is a test report');
@@ -17,8 +43,10 @@ describe('client-shorts-feature e2e test', () => {
       });
   
       // TODO Recreate when nav is updated
-      it.skip('tests navigating between tabs', () => {
+      it('tests navigating between tabs', () => {
         cy.get('#curBtn').should('contain.text', '1');
+        if(cy.get('#nextBtn').contains('Next')){
+        };
         cy.get('.formbuttonblue').contains('Next').click();
         cy.get('#curBtn').should('contain.text', '2');
         cy.get('.formbuttonblue').contains('Prev').click();
@@ -26,7 +54,7 @@ describe('client-shorts-feature e2e test', () => {
       });
     });
   
-    describe('upload component tests', () => {
+    describe.skip('upload component tests', () => {
       beforeEach(() => cy.visit('/shorts/upload'));
   
       it('should display then cancel upload component', () => {
