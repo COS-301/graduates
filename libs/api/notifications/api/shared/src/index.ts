93cb92b9c497f3dba5eabd100491932b444f8c1c
Richard_20430168 <u20430168@tuks.co.za>
2022-04-18 13:00:33 +0200

    :construction: Wrote nicer Api errors, still not perfect

--- libs/api/notifications/api/shared/src/index.ts
+++ libs/api/notifications/api/shared/src/index.ts
@@ -1,5 +1,7 @@
 export * from './lib/api-notifications-api-shared.entity';
 export * from './lib/api-notifications-api-shared-user.entity';
 export * from './lib/api-notifications-api-shared-email.entity';
+export * from './lib/api-notifications-api-shared-error.entity';
+
 
 
