68bccc504f1b22a4f9e3e5a195bb67e3e03bdf86
Michael du Plessis <mdp0101@gmail.com>
2022-04-18 09:59:29 +0200

    :bug: Fixed testing file imports

--- libs/api/access-status/api/feature/src/lib/api-access-status.module.ts
+++ libs/api/access-status/api/feature/src/lib/api-access-status.module.ts
@@ -1,8 +1,9 @@
 import { AccessStatusRepository } from '@graduates/api/access-status/repository/feature';
-import { AccessStatusService } from '@graduates/api/access-status/service/feature';
 import { PrismaService } from '@graduates/api/shared/services/prisma/data-access';
 import { Module } from '@nestjs/common';
 import { ApiAccessStatusResolver } from './api-access-status.resolver';
+import { AccessStatusService } from '@graduates/api/access-status/service/feature';
+import { forwardRef } from '@nestjs/common';
 
 @Module({
   controllers: [],
