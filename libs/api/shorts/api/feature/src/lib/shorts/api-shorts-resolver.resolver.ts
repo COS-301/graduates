12fc0efb44dddbb0256381e9cbd40dd98602a01a
Benjamin Osmers <73130428+BenjaminOsmers@users.noreply.github.com>
2022-04-18 17:11:51 +0200

    🔥 (Shorts-API): Added file upload

--- libs/api/shorts/api/feature/src/lib/shorts/api-shorts-resolver.resolver.ts
+++ libs/api/shorts/api/feature/src/lib/shorts/api-shorts-resolver.resolver.ts
@@ -20,13 +20,19 @@ import {
 } from '@graduates/api/shorts/service/feature';
 import { NotFoundException } from '@nestjs/common';
 import { AuthenticationUser } from '@graduates/api/authentication/api/shared/interfaces/data-access';
+import uuid from 'uuid';
+import {
+  FirebaseService,
+  FirebaseFolders,
+} from '@graduates/api/storage/repository/data-access';
 
 @Resolver(Short)
 export class ShortsResolver {
   constructor(
     private readonly service: ShortsService,
     private readonly tagsService: ShortsTagsService,
-    private readonly reportsService: ShortsReportsService
+    private readonly reportsService: ShortsReportsService,
+    private readonly fbService: FirebaseService
   ) {}
 
   @ResolveField()
@@ -90,10 +96,11 @@ export class ShortsResolver {
   async getShortsByTag(@Args('tagId') tagId: string): Promise<Short[]> {
     return await this.service.findShortsByTag(tagId);
   }
-  @Query(() =>String) 
-  pingShorts(){
-    return "on";
+  @Query(() => String)
+  pingShorts() {
+    return 'on';
   }
+
   /**
    * Mutation to create a short
    * @param {ShortCreateInput} short The short to create
@@ -103,10 +110,42 @@ export class ShortsResolver {
   @Mutation(() => Short)
   async createShort(
     @Args('short') short: ShortCreateInput,
-    @Args('userId') userId: string
+    @Args('userId') userId: string,
+    @Args('vidString') vidString: string,
+    @Args('thumbString') thumbString: string,
+    @Args('vidCat', { type: () => FirebaseFolders }) vidCat: FirebaseFolders,
+    @Args('thumbCat', { type: () => FirebaseFolders }) thumbCat: FirebaseFolders
   ): Promise<Short | null> {
-    const res = await this.service.createShort(short, userId);
-    return res;
+    const vidName = uuid.v4();
+    const thumbName = uuid.v4();
+
+    await this.fbService
+      .uploadAsBase64String(vidString, vidName, vidCat)
+      .then((res) => {
+        if (res) {
+          this.fbService.getURLByName(vidName, vidCat).then((res) => {
+            const vidRef = res;
+
+            this.fbService
+              .uploadAsBase64String(thumbString, thumbName, thumbCat)
+              .then((res) => {
+                if (res) {
+                  this.fbService.getURLByName(vidName, vidCat).then((res) => {
+                    const thumbRef = res;
+                    return this.service.createShort(
+                      short,
+                      userId,
+                      vidRef,
+                      thumbRef
+                    );
+                  });
+                }
+              });
+          });
+        }
+      });
+
+    return null;
   }
 
   /**
