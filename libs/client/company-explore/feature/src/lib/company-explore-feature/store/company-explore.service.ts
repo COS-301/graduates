71e58ff2bb2d6ec0a224009351e757560142c5f4
Trunks29 <u17169811@tuks.co.za>
2022-04-18 11:14:24 +0200

    fixed some test issues and included the NavBar

--- libs/client/company-explore/feature/src/lib/company-explore-feature/store/company-explore.service.ts
+++ libs/client/company-explore/feature/src/lib/company-explore-feature/store/company-explore.service.ts
@@ -1,17 +1,33 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Company } from './company-model';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
     providedIn: 'root',
 })
-export class CompanyExploreService {
+export class CompanyExploreService 
+{
 
-    constructor(private http: HttpClient) {
-       
-    }
+    constructor(private http: HttpClient) { }
 
-    fetchCompanies() {
-        return this.http.get<Company[]>('http://localhost:3000/companies');
-    }
-}
+    fetchCompanies():Observable<any> {
+        const query =
+          "query {getListOfComapnies{id,name,UserProfile{profilePicture}}}";
+    
+        const options = {
+          headers: new HttpHeaders({
+            "Content-Type": "application/json",
+          }),
+        };
+    
+        return this.http.post<any>(
+          "http://localhost:3333/graphql",
+          JSON.stringify({
+            query: query,
+          }),
+          options
+        );
+      }
+}    
