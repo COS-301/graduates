d6a9fb5a9ba289ab5a727292b6dd3814f65e5887
Dylan Whiteford <93826822+DYL-WF@users.noreply.github.com>
2022-04-20 09:42:01 +0200

    :bug: .length() changed to .length

--- libs/client/shorts/feature/src/lib/story-explore/story-explore.component.ts
+++ libs/client/shorts/feature/src/lib/story-explore/story-explore.component.ts
@@ -252,7 +252,7 @@ export class StoryExploreComponent implements OnInit {
     const output = s.split('#');
     output.shift();
 
-    if(output.length() <=0) return "shortTag: []";
+    if(output.length <=0) return "shortTag: []";
     let out = "shortTag: [";
 
     for (let index = 0; index < output.length; index++) {
