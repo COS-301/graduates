b0c71b5f4c5c5da71b3abb20d01d24e7de00b511
David Roodt <davidroodt2@gmail.com>
2022-04-20 14:11:04 +0200

    get Tags Test

--- libs/api/student-profiles/service/feature/src/lib/api-student-profiles.service.spec.ts
+++ libs/api/student-profiles/service/feature/src/lib/api-student-profiles.service.spec.ts
@@ -77,19 +77,51 @@ describe('ApiStudentProfileService', () => {
     })
   });
 
-describe('getBio', () => {
-  const userdata = mockStudent.bio;
-  it('should return the student\'s bio', async () => {
-    jest
-      .spyOn(service, 'getBio')
-      .mockImplementation(
-        (): Promise<string> => {
-          return Promise.resolve(userdata);
-        }
-      );
-      const userID = await repo.getUserIDFromStudentNumber('u12345678');
-      const result: Promise<string> = await service.getBio(userID);
-      expect(result).toEqual(userdata);
-  })
-});
+  describe('getBio', () => {
+    const userdata = mockStudent.bio;
+    it('should return the student\'s bio', async () => {
+      jest
+        .spyOn(service, 'getBio')
+        .mockImplementation(
+          (): Promise<string> => {
+            return Promise.resolve(userdata);
+          }
+        );
+        const userID = await repo.getUserIDFromStudentNumber('u12345678');
+        const result: Promise<string> = await service.getBio(userID);
+        expect(result).toEqual(userdata);
+    })
+  });
+
+  describe('getBio', () => {
+    const userdata = mockStudent.bio;
+    it('should return the student\'s bio', async () => {
+      jest
+        .spyOn(service, 'getBio')
+        .mockImplementation(
+          (): Promise<string> => {
+            return Promise.resolve(userdata);
+          }
+        );
+        const userID = await repo.getUserIDFromStudentNumber('u12345678');
+        const result: Promise<string> = await service.getBio(userID);
+        expect(result).toEqual(userdata);
+    })
+  });
+
+  describe('getTags', () => {
+    const userdata = mockStudent.tags;
+    it('should return the student\'s bio', async () => {
+      jest
+        .spyOn(service, 'getTags')
+        .mockImplementation(
+          (): Promise<string[]> => {
+            return Promise.resolve(userdata);
+          }
+        );
+        const userID = await repo.getUserIDFromStudentNumber('u12345678');
+        const result: Promise<string> = await service.getTags(userID);
+        expect(result).toEqual(userdata);
+    })
+  });
 });
