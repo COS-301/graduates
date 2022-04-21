6965ee3e6d0d49b9b2819db441f8bb98eff6ee9f
Lehlohonolo-Sehako <u20557622@tuks.co.za>
2022-04-21 17:12:21 +0200

    :bug: (Tester): Fixed e2e test conflicts and bugs

--- apps/client-e2e/src/integration/app.spec.ts
+++ apps/client-e2e/src/integration/app.spec.ts
@@ -180,7 +180,7 @@ describe('Company Representative Feature', () => {
 
   it('should allow representative to login', () => {
 
-      cy.get('.inputCss required').type('ishe.dzingirai@gmail.com');
+      cy.get('input[type=email]').type('ishe.dzingirai@gmail.com');
       cy.get('.password').type('${IamACSStudent@1}{enter}');
       cy.get('.btn btn-primary').click().visit('/CompanyRepresentativeHome');
   
@@ -196,7 +196,7 @@ describe('Company Representative Update Details', () => {
 
       it('should allow representative to update details', () => {
           cy.get('#fname').type('Tester Role');
-          cy.get('#title').type('Software Tester');
+          cy.get('.title').type('Software Tester');
           cy.get('textarea[name=experience]').type('Beginner');
           cy.get('textarea[name=about]').type('Looking for Graduate Machine Learning Engineers');
           cy.get('#number').type('0812347623');
