6b346827ec27763574e7594f03d57e28934983c8
SimphiweNdlovu <93398304+SimphiweNdlovu@users.noreply.github.com>
2022-04-21 23:54:25 +0200

    ✅ (unit test): adding unit test

--- libs/api/storage/api/feature/src/lib/api-storage.resolver.spec.ts
+++ libs/api/storage/api/feature/src/lib/api-storage.resolver.spec.ts
@@ -1,6 +1,12 @@
 import { ApiStorageServiceFeatureModule } from '@graduates/api/storage/service/feature';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApiStorageResolver } from './api-storage.resolver';
+import { ApiStorage } from '@graduates/api/storage/api/shared/data-access';
+import { QueryBus } from '@nestjs/cqrs';
+import { prisma } from '@prisma/client';
+
+const MockApiImpl : jest.Mocked<ApiStorage> = new ApiStorage() as ApiStorage;
+
 
 describe('ApStorageResolver', () => {
   let resolver:ApiStorageResolver;
@@ -17,6 +23,65 @@ describe('ApStorageResolver', () => {
     expect(resolver).toBeDefined();
   });
 });
+
+describe('ApiStorageResolver', () => {
+  let resolver: ApiStorageResolver;
+  //let queryBus: QueryBus;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ApiStorageResolver,
+        ApiStorageServiceFeatureModule,
+        ApiStorage,
+        QueryBus,
+      ],
+    }).compile();
+    //queryBus = module.get<QueryBus>(QueryBus);
+    resolver = module.get<ApiStorageResolver>(ApiStorageResolver);
+  });
+
+it('Should be thruthy', async () => {
+  jest
+    .spyOn(resolver, 'upload')
+    .mockImplementation(
+      (): Promise< boolean> =>
+        Promise.resolve(true)
+    );
+    expect(resolver.upload).not.toHaveBeenCalled();
+  
+  expect(await resolver.upload("MuzisCV","1","CV","PDF","blob")).toBeTruthy();
+  expect(resolver.upload).toHaveBeenCalled();
+});
+
+it('To be string or thurthy', async () => {
+  jest
+    .spyOn(resolver, 'download')
+    .mockImplementation(
+      (): Promise<string | boolean> =>
+        Promise.resolve(true)
+    );
+    expect(resolver.download).not.toHaveBeenCalled();
+
+  expect(await resolver.download("1","CV")).toBeTruthy();
+  expect(resolver.download).toHaveBeenCalled();
+});
+
+it('To be truthy delete', async () => {
+  jest
+    .spyOn(resolver, 'delete')
+    .mockImplementation(
+      (): Promise<number> =>
+        Promise.resolve(1)
+    );
+
+    expect(resolver.delete).not.toHaveBeenCalled();
+  expect(await resolver.delete("1","CV")).toBeTruthy();
+  expect(resolver.delete).toHaveBeenCalled();
+});
+
+});
+
+
 /*const record = {fileCategory:"CV" , userID:"u20469366"}
 class ApiStorageServiceMock {
   deleteFile(userID: string, fileCategory: string) {
