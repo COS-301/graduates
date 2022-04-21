a75e4c3424f6155a239e821b9051cc772796fdb0
Khotso-Bore <u19180642@tuks.co.za>
2022-04-21 11:11:39 +0200

    fixed: file reference issue

--- libs/api/authorization/service/feature/src/index.ts
+++ libs/api/authorization/service/feature/src/index.ts
@@ -1,5 +1,5 @@
 export * from './lib/api-authorization-service-feature.module';
-export * from './lib/queries/handlers/get-companyid-permissions.handler';
+export * from './lib/queries/handlers/get-companyId-permissions.handler';
 export * from './lib/queries/handlers/get-delete-permissions.handler';
 export * from './lib/queries/handlers/get-edit-permissions.handler';
 export * from './lib/queries/handlers/get-role-permissions.handler';
