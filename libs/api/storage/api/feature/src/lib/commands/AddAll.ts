4c61511d79787e3e94e682fde72d977d734c30d5
lindzik <94564461+lindzik@users.noreply.github.com>
2022-04-21 12:43:36 +0200

    Delete AddAll.ts

--- libs/api/storage/api/feature/src/lib/commands/AddAll.ts
+++
@@ -1,8 +0,0 @@
-export class AddAll {
-    constructor(
-        public readonly MimeType: string,
-        public readonly UserId: number,
-        public readonly UserPath: string,
-
-    ) { }
-}
