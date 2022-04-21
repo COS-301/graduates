ad7c31ad523cd8903677adad9fd671dc18f32657
Khotso-Bore <u19180642@tuks.co.za>
2022-04-21 10:55:53 +0200

    fixed: added modules to index.ts

--- libs/api/authorization/api/feature/src/lib/api-authorization-api-feature.module.ts
+++ libs/api/authorization/api/feature/src/lib/api-authorization-api-feature.module.ts
@@ -6,19 +6,17 @@ import { ApiAuthorizationService } from '@graduates/api/authorization/service/fe
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { ApiAuthorization } from '../../../shared/src/lib/api-authorization.entity';
 import { CqrsModule, QueryBus } from '@nestjs/cqrs';
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { GetRoleQueryPermissionHandler } from 'libs/api/authorization/service/feature/src/lib/queries/handlers/get-role-permissions.handler';
 import { PrismaService } from '@graduates/api/shared/services/prisma/data-access';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { Adminauthorization } from 'libs/api/authorization/repository/data-access/src/lib/api-authorization-repository-admin.repository';
+import { Adminauthorization } from '@graduates/api/authorization/repository/data-access';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { GetEditPermissionHandler } from 'libs/api/authorization/service/feature/src/lib/queries/handlers/get-edit-permissions.handler';
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { GetDeletePermissionHandler } from 'libs/api/authorization/service/feature/src/lib/queries/handlers/get-delete-permissions.handler';
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { GetViewPermissionHandler } from 'libs/api/authorization/service/feature/src/lib/queries/handlers/get-view-permissions.handler';
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { GetCompanyIdHandler } from 'libs/api/authorization/service/feature/src/lib/queries/handlers/get-companyId-permissions.handler';
+import {
+  GetRoleQueryPermissionHandler,
+  GetCompanyIdHandler,
+  GetViewPermissionHandler,
+  GetDeletePermissionHandler,
+  GetEditPermissionHandler,
+} from '@graduates/api/authorization/service/feature';
 
 @Module({
   controllers: [],
