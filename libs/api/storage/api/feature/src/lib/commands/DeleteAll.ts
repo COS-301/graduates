446920b357ec7c5b6c3db7991da87c2006f0bcd7
lindzik <94564461+lindzik@users.noreply.github.com>
2022-04-21 12:50:16 +0200

    Delete DeleteAll.ts

--- libs/api/storage/api/feature/src/lib/commands/DeleteAll.ts
+++
@@ -1,8 +0,0 @@
-export class DeleteAll {
-    constructor(
-        public readonly MimeType: string,
-        public readonly UserId: number,
-        public readonly UserPath: string,
-
-    ) { }
-}
